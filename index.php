721456746ba6baa317aefb046f5e3d42cd0a771b
Diego Rascón <joserascon2002@gmail.com>
2024-04-02 20:37:52 -0700

    Se implementó el funcionamiento correcto de la barra de navegación

--- index.php
+++ index.php
@@ -5,45 +5,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Capacitación Docente - ITESCA</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="icon" href="assets/images/icono-itesca.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/custom.css">
-    <link rel="icon" href="assets/images/icono-itesca.png">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script defer src="assets/js/routing.js"></script>
 </head>
 
 <body>
     <header>
-        <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary shadow-sm">
+        <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary shadow-sm user-select-none">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
+                <div class="navbar-brand">
                     <img src="assets/images/icono-itesca.png" alt="Logo de ITESCA" width="29" class="d-inline-block align-text-top">
                     <span>Capacitación Docente</span>
-                </a>
+                </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav nav-underline me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Oferta</a>
+                            <a href="#" class="nav-link" id="oferta" aria-current="page">Oferta</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Inscritos</a>
+                            <a href="#" class="nav-link" id="inscritos">Inscritos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Instruyendo</a>
+                            <a href="#" class="nav-link" id="instruyendo">Instruyendo</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-target="horario">Horario</a>
+                            <a href="#" class="nav-link" id="horario">Horario</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Historial</a>
+                            <a href="#" class="nav-link" id="historial">Historial</a>
                         </li>
                     </ul>
                     <div class="dropdown">
-                        <button class="btn btn-outline-secondary d-flex align-items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="fs-6 me-2">José Luis Beltrán Márquez</span>
+                        <button class="w-100 btn btn-outline-secondary d-flex align-items-center justify-content-end" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="fs-6 me-2">User</span>
                             <i class="bi bi-person-circle" style="font-size: 1rem;"></i>
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end">
@@ -60,24 +62,7 @@
         </nav>
     </header>
     <main class="main-container">
-        <div id="content" class="container py-4">
-            <h1>Hello World!</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ullam sapiente repellendus illo vel unde culpa voluptatum necessitatibus temporibus. Fugit in minus architecto mollitia hic amet dignissimos labore voluptas distinctio.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A veritatis nam id fuga! Praesentium illum nisi minus eos officia neque temporibus veritatis cum impedit in fugiat quae, ab consequuntur aut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatem dolores sapiente voluptates, at corrupti similique veritatis vero animi omnis repellat quidem. Doloribus minus aliquam molestiae laborum saepe repudiandae? Explicabo.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia cum quas iste delectus consectetur perspiciatis voluptatem quidem non iusto pariatur fuga omnis distinctio enim, tempora inventore quam debitis quod?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae, sunt voluptatum voluptates numquam animi perspiciatis minus aliquam at ratione sit quas deserunt, natus eum architecto a consequatur eaque ad!</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ullam sapiente repellendus illo vel unde culpa voluptatum necessitatibus temporibus. Fugit in minus architecto mollitia hic amet dignissimos labore voluptas distinctio.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A veritatis nam id fuga! Praesentium illum nisi minus eos officia neque temporibus veritatis cum impedit in fugiat quae, ab consequuntur aut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatem dolores sapiente voluptates, at corrupti similique veritatis vero animi omnis repellat quidem. Doloribus minus aliquam molestiae laborum saepe repudiandae? Explicabo.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia cum quas iste delectus consectetur perspiciatis voluptatem quidem non iusto pariatur fuga omnis distinctio enim, tempora inventore quam debitis quod?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae, sunt voluptatum voluptates numquam animi perspiciatis minus aliquam at ratione sit quas deserunt, natus eum architecto a consequatur eaque ad!</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ullam sapiente repellendus illo vel unde culpa voluptatum necessitatibus temporibus. Fugit in minus architecto mollitia hic amet dignissimos labore voluptas distinctio.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A veritatis nam id fuga! Praesentium illum nisi minus eos officia neque temporibus veritatis cum impedit in fugiat quae, ab consequuntur aut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatem dolores sapiente voluptates, at corrupti similique veritatis vero animi omnis repellat quidem. Doloribus minus aliquam molestiae laborum saepe repudiandae? Explicabo.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia cum quas iste delectus consectetur perspiciatis voluptatem quidem non iusto pariatur fuga omnis distinctio enim, tempora inventore quam debitis quod?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae, sunt voluptatum voluptates numquam animi perspiciatis minus aliquam at ratione sit quas deserunt, natus eum architecto a consequatur eaque ad!</p>
-        </div>
+        <div id="page" class="container py-4"></div>
     </main>
 </body>
 
