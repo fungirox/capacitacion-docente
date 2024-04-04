320c73050d7e8b720911effd6108840b946c566d
Diego Rascón <joserascon2002@gmail.com>
2024-04-03 19:02:20 -0700

    Pladeholder de cards

--- templates/oferta.php
+++ templates/oferta.php
@@ -1,16 +1,47 @@
-<h1>Hello World!</h1>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ullam sapiente repellendus illo vel unde culpa voluptatum necessitatibus temporibus. Fugit in minus architecto mollitia hic amet dignissimos labore voluptas distinctio.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A veritatis nam id fuga! Praesentium illum nisi minus eos officia neque temporibus veritatis cum impedit in fugiat quae, ab consequuntur aut!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatem dolores sapiente voluptates, at corrupti similique veritatis vero animi omnis repellat quidem. Doloribus minus aliquam molestiae laborum saepe repudiandae? Explicabo.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia cum quas iste delectus consectetur perspiciatis voluptatem quidem non iusto pariatur fuga omnis distinctio enim, tempora inventore quam debitis quod?</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae, sunt voluptatum voluptates numquam animi perspiciatis minus aliquam at ratione sit quas deserunt, natus eum architecto a consequatur eaque ad!</p>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ullam sapiente repellendus illo vel unde culpa voluptatum necessitatibus temporibus. Fugit in minus architecto mollitia hic amet dignissimos labore voluptas distinctio.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A veritatis nam id fuga! Praesentium illum nisi minus eos officia neque temporibus veritatis cum impedit in fugiat quae, ab consequuntur aut!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatem dolores sapiente voluptates, at corrupti similique veritatis vero animi omnis repellat quidem. Doloribus minus aliquam molestiae laborum saepe repudiandae? Explicabo.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia cum quas iste delectus consectetur perspiciatis voluptatem quidem non iusto pariatur fuga omnis distinctio enim, tempora inventore quam debitis quod?</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae, sunt voluptatum voluptates numquam animi perspiciatis minus aliquam at ratione sit quas deserunt, natus eum architecto a consequatur eaque ad!</p>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ullam sapiente repellendus illo vel unde culpa voluptatum necessitatibus temporibus. Fugit in minus architecto mollitia hic amet dignissimos labore voluptas distinctio.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A veritatis nam id fuga! Praesentium illum nisi minus eos officia neque temporibus veritatis cum impedit in fugiat quae, ab consequuntur aut!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatem dolores sapiente voluptates, at corrupti similique veritatis vero animi omnis repellat quidem. Doloribus minus aliquam molestiae laborum saepe repudiandae? Explicabo.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quia cum quas iste delectus consectetur perspiciatis voluptatem quidem non iusto pariatur fuga omnis distinctio enim, tempora inventore quam debitis quod?</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae, sunt voluptatum voluptates numquam animi perspiciatis minus aliquam at ratione sit quas deserunt, natus eum architecto a consequatur eaque ad!</p>
+<h1>Oferta de Cursos</h1>
+<div class="my-2 row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+        <div class="card h-100">
+            <img src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="150px" style="object-fit:cover;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+                Card footer
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card h-100">
+            <img src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="150px" style="object-fit:cover;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a short card.</p>
+            </div>
+            <div class="card-footer">
+                <span class="badge text-bg-primary">Etiqueta 1</span>
+                <span class="badge text-bg-success">Etiqueta 2</span>
+                <span class="badge text-bg-danger">Etiqueta 3</span>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card h-100">
+            <img src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="150px" style="object-fit:cover;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card h-100">
+            <img src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="150px" style="object-fit:cover;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+    </div>
+</div>
