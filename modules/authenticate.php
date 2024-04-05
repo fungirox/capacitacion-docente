6221625a4611e78dd65b0fb1dd39c027ef64d835
Diego Rascón <joserascon2002@gmail.com>
2024-04-04 21:14:28 -0700

    Esqueleto de la funcionalidad para iniciar sesión

---
+++ modules/authenticate.php
@@ -0,0 +1,23 @@
+<?php
+require_once "../app-code/config.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $user = $_POST["user"];
+    $password = $_POST["password"];
+
+    try {
+        if ($user === 'sa' && $password === 'sa') {
+            $_SESSION["loggedIn"] = true;
+            header("Location: ../");
+            die();
+        } else {
+            header("Location: ../login.php");
+            die();
+        }
+    } catch (exception) {
+        die("Query failed");
+    }
+} else {
+    header("Location: ../login.php");
+    die();
+}
