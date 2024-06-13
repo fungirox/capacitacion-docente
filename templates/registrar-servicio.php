952e9c7d28cb44d1f7b358141986acbfcd56118d
fungirox <annxrk13@gmail.com>
2024-06-12 23:34:58 -0700

    Prototipo de "add-servicio", falta agregar horario semanal y realizar el registro en la base de datos

--- templates/registrar-servicio.php
+++ templates/registrar-servicio.php
@@ -1,4 +1,28 @@
 <?php
+include '../config/connection.php';
+
+$teachersQuery = "SELECT u.USERID, d.DOCENTEID, u.USER_Nombre, u.USER_Apellido FROM dbo.tblUsuario as u INNER JOIN dbo.tblDocente as d ON u.USERID = d.USERID;";
+$areaQuery = "SELECT * FROM dbo.tblArea;";
+$stnt = $connection->prepare($teachersQuery);
+$stnt->execute();
+$teachersData = $stnt->fetchAll();
+$stnt = $connection->prepare($areaQuery);
+$stnt->execute();
+$areaData = $stnt->fetchAll();
+
+$todayDate = new DateTime();
+$formattedToday = $todayDate->format('Y-m-d');
+$tomorrowDate = new DateTime('tomorrow');
+$formattedTomorrow = $tomorrowDate->format('Y-m-d');
+
+$weekdays = [
+    'lunes' => 'Lunes',
+    'martes' => 'Martes',
+    'miercoles' => 'Miércoles',
+    'jueves' => 'Jueves',
+    'viernes' => 'Viernes'
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,46 +37,78 @@
     <h1>Registrar servicio</h1>
     <form>
         <!--- Nombre del servicio --->
-        <label for="curso_name">Nombre de servicio</label>
-        <input type="text" id="curso_name" required placeholder="Nombre del curso" name="curso_name" />
+        <label for="service-name">Nombre de servicio</label>
+        <input type="text" required placeholder="Nombre del curso" id="service-name" name="service-name" />
         <br />
         <!--- Tipo de servicio: curso, taller y diplomado --->
-        <label for="curso_type">Tipo de servicio</label>
-        <input type="radio" id="curso" required name="curso_type" value="curso" checked="checked" />
+        <label for="service-type">Tipo de servicio</label>
+        <input type="radio" required name="service-type" id="curso" value="curso" checked="checked" />
         <label for="curso">Curso</label>
 
-        <input type="radio" id="taller" required name="curso_type" value="taller" />
+        <input type="radio" required name="service-type" id="taller" value="taller" />
         <label for="taller">Taller</label>
 
-        <input type="radio" id="diplomado" required name="curso_type" value="diplomado" />
+        <input type="radio" required name="service-type" id="diplomado" value="diplomado" />
         <label for="diplomado">Diplomado</label>
         <br />
         <!--- Tipo de servicio: interno, externo --->
-        <input type="radio" id="interno" required name="curso_type_2" value="interno" checked="checked" />
+        <input type="radio" required name="service-category" id="interno" value="0" checked="checked" />
         <label for="interno">Interno</label>
 
-        <input type="radio" id="externo" required name="curso_type_2" value="externo" />
+        <input type="radio" required name="service-category" id="externo" value="1" />
         <label for="externo">Externo</label>
         <br />
         <!--- Seleccionar instructor --->
-        <label for="docentes">Instructores</label>
-        <select name="docentes" id="docentes">
-            <option value="docente_1" selected="true">José Luis Beltran Marquez</option>
-            <option value="docente_2">Manuel Ramirez Lopez</option>
-            <option value="docente_3">Dagoberto Ramirez Rendón</option>
+        <!--- Se selecciona desde la tabla docentes porque todos los docentes pueden ser instructores, 
+            estos no son instructores hasta que sean asignados a un curso --->
+        <label for="teachers">Instructores</label>
+        <select name="teachers" id="teachers">
+            <?php foreach ($teachersData as $row) : ?>
+                <?php
+                $userID = htmlspecialchars($row['USERID'], ENT_QUOTES, 'UTF-8');
+                $userFirstName = htmlspecialchars($row['USER_Nombre'], ENT_QUOTES, 'UTF-8');
+                $userLastName = htmlspecialchars($row['USER_Apellido'], ENT_QUOTES, 'UTF-8');
+                ?>
+                <option value="<?= $userID ?>"><?= $userFirstName ?> <?= $userLastName ?></option>
+            <?php endforeach; ?>
         </select>
         <button>Agregar instructor</button>
         <br />
         <!-- Aquí falta lo de areas, aun no sé como hacerlo -->
-        <p>Áreas</p>
+        <label for="areas">Areas</label>
+        <br />
+        <?php foreach ($areaData as $row) : ?>
+            <?php
+            $areaSiglas = htmlspecialchars($row['AREA_Siglas'], ENT_QUOTES, 'UTF-8');
+            $areaID = htmlspecialchars($row['AREAID'], ENT_QUOTES, 'UTF-8');
+            ?>
+            <input type="checkbox" name="areas" value="<?= $areaID ?>"><?= $areaSiglas ?><br />
+        <?php endforeach; ?>
+
         <br />
         <!-- Horario -->
-        <label for="presencial">Horas presenciales</label>
-        <input type="number" min="0" id="presencial" required value="0" name="presencial" />
-        <label for="virtual">Horas virtuales</label>
-        <input type="number" min="0" id="virtual" required value="0" name="virtual" />
+        <label for="physical-hours">Horas presenciales</label>
+        <input type="number" min="0" required value="0" id="physical-hours" name="physical-hours" />
+        <label for="virtual-hours">Horas virtuales</label>
+        <input type="number" min="0" required value="0" id="virtual-hours" name="virtual-hours" />
         <br />
-
+        <!-- Fechas -->
+        <label for="start-date">Fecha de inicio</label>
+        <input type="date" id="start-date" name="start-date" min="<?= $formattedToday; ?>" value="<?= $formattedToday; ?>"> <br />
+        <label for="finish-date">Fecha de finalización</label>
+        <input type="date" id="finish-date" name="finish-date" min="<?= $formattedToday; ?>" value="<?= $formattedTomorrow; ?>"> <br />
+        <!--- Aula -->
+        <!-- Si las horas presenciales son 0, no debe solicitar aula, en caso contrario es required -->
+        <label for="classroom">Aula</label>
+        <input type="number" min="0" id="classroom" name="classroom" />
+        <br />
+        <!--- Días de la semana -->
+        <label for="week">Días de la semana</label> <br />
+        <?php foreach ($weekdays as $id => $name) : ?>
+            <input type="checkbox" id="<?= $id ?>" name="weekday[]" value="<?= $id ?>">
+            <label for="<?= $id ?>"><?= $name ?></label><br />
+        <?php endforeach; ?>
+        <br />
     </form>
 </body>
 
