8bfa980a3cff91e968809bc164fa91b8d32acae8
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-16 20:07:54 -0700

    Se usa la forma buena de imprimir los datos del ciclo

--- templates/oferta.php
+++ templates/oferta.php
@@ -33,6 +33,7 @@ require_once "../modules/courses/getAllCourses.php";
 <div class="my-2 row row-cols-1 row-cols-md-3 g-4">
     <?php foreach ($allCourses as $course) : ?>
         <?php
+        $id = $course["CURSOID"];
         $nombre = htmlspecialchars($course["CURSO_Nombre"], ENT_QUOTES, "UTF-8");
         ?>
         <div class="col">
@@ -40,7 +41,7 @@ require_once "../modules/courses/getAllCourses.php";
                 <img src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="150px" style="object-fit:cover;">
                 <div class="card-body">
                     <h5 class="card-title"><?= $nombre ?></h5>
-                    <p class="card-text">sadasdsadsa</p>
+                    <p class="card-text">sadasdsadsaaaaaaaaaaa</p>
                 </div>
                 <div class="card-footer">
                     Card footer
