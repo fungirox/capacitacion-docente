4fe9fabe2f9fe2362c8d83e8e6814f328343eb88
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-16 20:07:24 -0700

    Módulo que hace fetch a todos los cursos

--- modules/courses/getAllCourses.php
+++ modules/courses/getAllCourses.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__."/../../config/connection.php";
+require_once __DIR__ . "/../../config/connection.php";
 
 $query = "SELECT * FROM tblCurso";
 $getAllCourses = $connection->prepare($query);
