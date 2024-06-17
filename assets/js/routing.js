2e1f05ad6d8c3598e6531e968e23e6f77c6c2f8a
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-16 20:06:36 -0700

    Se reformateó el código

--- assets/js/routing.js
+++ assets/js/routing.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    $('#page').load('templates/oferta.php');
-    $('#oferta').addClass('active').attr('aria-current', 'page');
+    $("#page").load("templates/oferta.php");
+    $("#oferta").addClass("active").attr("aria-current", "page");
 
-    $('.nav-link').click(function (event) {
+    $(".nav-link").click(function (event) {
         event.preventDefault();
-        let page = 'templates/' + $(this).attr('id') + '.php';
-        $('#page').load(page);
-        $('.nav-link').removeClass('active').removeAttr('aria-current');
-        $(this).addClass('active').attr('aria-current', 'page');
+        let page = "templates/" + $(this).attr("id") + ".php";
+        $("#page").load(page);
+        $(".nav-link").removeClass("active").removeAttr("aria-current");
+        $(this).addClass("active").attr("aria-current", "page");
     });
 });
