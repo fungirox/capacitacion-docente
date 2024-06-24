5ec36ff10f056d2b93100d02ce1ff0bf5eb742b2
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-23 18:49:05 -0700

    Ahora se maneja la lógica del back en controladores

--- modules/courses/getAllCourses.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . "/../../config/connection.php";
-
-$query = "SELECT * FROM tblCurso";
-$getAllCourses = $connection->prepare($query);
-$getAllCourses->execute();
-$allCourses = $getAllCourses->fetchAll(PDO::FETCH_ASSOC);
