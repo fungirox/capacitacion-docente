46fb69c4e35406ef20a2edb5c16676a6909bcf71
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-23 18:41:16 -0700

    Se separó la lógica del curso en el controlador y la vista

---
+++ controllers/curso.php
@@ -0,0 +1,9 @@
+<?php
+$db = new Database();
+$id = $_GET["id"];
+$course = $db->query("SELECT * FROM tblCurso where CURSOID = ?", [$id])->fetch(PDO::FETCH_ASSOC);
+
+$id = $course["CURSOID"];
+$nombre = htmlspecialchars($course["CURSO_Nombre"], ENT_QUOTES, "UTF-8");
+
+require  "views/curso.view.php";
---
+++ views/curso.view.php
@@ -0,0 +1,14 @@
+<?php
+require 'components/header.php';
+require 'components/navbar.php';
+?>
+<main class="main-container">
+    <div id="page" class="container py-4">
+        <h1><?= $nombre ?></h1>
+    </div>
+</main>
+<hr class="container">
+<footer class="py-4">
+    <p class="text-center text-body-secondary">© Copyright 2024 ITESCA - Todos los Derechos Reservados</p>
+</footer>
+<?php require 'components/footer.php'; ?>
