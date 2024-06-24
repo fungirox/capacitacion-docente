d0dba81af11f5a4b82b046ca6ca48f7c3553a40a
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-23 18:33:47 -0700

    Componente para manejar las rutas que no se encontraron

---
+++ views/errors/404.php
@@ -0,0 +1,11 @@
+<?php require "views/components/header.php"; ?>
+<main class="main-container">
+    <div id="page" class="container py-4 text-center">
+        <div>
+            <h1>Error 404</h1>
+            <p>No se encontró la ruta deseada</p>
+            <a href="<?= $baseUrl ?>/">Volver al inicio</a>
+        </div>
+    </div>
+</main>
+<?php require 'views/components/footer.php'; ?>
