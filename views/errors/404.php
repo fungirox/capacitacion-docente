7c0dde3787ebc4e36dfd621f61332ed46510e822
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-25 15:32:44 -0700

    Se reestructuró la interfaz

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,11 +1,7 @@
 <?php require "views/components/header.php"; ?>
-<main class="main-container">
-    <div id="page" class="container py-4 text-center">
-        <div>
-            <h1>Error 404</h1>
-            <p>No se encontró la ruta deseada</p>
-            <a href="<?= $baseUrl ?>/">Volver al inicio</a>
-        </div>
-    </div>
+<main role="main" class="container text-center pt-4">
+    <h1>Error 404</h1>
+    <p>No se encontró la ruta deseada</p>
+    <a href="<?= $baseUrl ?>/">Volver al inicio</a>
 </main>
-<?php require 'views/components/footer.php'; ?>
+<?php require "views/components/footer.php"; ?>
