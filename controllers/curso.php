07bef64016e2073a67c35ad8df41172c1c92010e
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-25 15:26:26 -0700

    Se establece el título de la página

--- controllers/curso.php
+++ controllers/curso.php
@@ -6,4 +6,6 @@ $course = $db->query("SELECT * FROM tblCurso where CURSOID = ?", [$id])->fetch(P
 $id = $course["CURSOID"];
 $nombre = htmlspecialchars($course["CURSO_Nombre"], ENT_QUOTES, "UTF-8");
 
+$title = $nombre;
+
 require  "views/curso.view.php";
--- controllers/oferta.php
+++ controllers/oferta.php
@@ -2,4 +2,6 @@
 $db = new Database();
 $allCourses = $db->query("SELECT * FROM tblCurso")->fetchAll(PDO::FETCH_ASSOC);
 
+$title = "Oferta";
+
 require  "views/oferta.view.php";
