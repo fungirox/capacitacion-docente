fca2d6a92832a9975c7f425c270a0ea4dae73a4a
Diego Rascón <jose.d.rascon@gmail.com>
2024-06-25 15:26:55 -0700

    Componente para footer con la información de derechos de autor de ITESCA

---
+++ views/components/styledFooter.php
@@ -0,0 +1,5 @@
+<hr class="container">
+<footer class="py-4">
+    <p class="text-center text-body-secondary">© Copyright 2024 ITESCA - Todos los Derechos Reservados</p>
+</footer>
+<?php require __DIR__ . "/footer.php"; ?>
