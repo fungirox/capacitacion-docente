d88b444f2ff54dc33eec9d4519bf4b7472ecb488
Diego Rascón <jose.d.rascon@gmail.com>
2024-07-02 22:37:02 -0700

    Se aborta si no se encuentra el curso y se agregó el pseudocódigo de la autenticación

--- controllers/curso.php
+++ controllers/curso.php
@@ -3,6 +3,14 @@ $db = new Database();
 $id = $_GET["id"];
 $course = $db->query("SELECT * FROM tblCurso where CURSOID = ?", [$id])->fetch();
 
+if (!$course) {
+    $abort();
+}
+
+// if ($course['idUsuario' !== $idUsuarioActual]) {
+//     abort(Response::FORBIDDEN);
+// }
+
 $id = $course["CURSOID"];
 $nombre = htmlspecialchars($course["CURSO_Nombre"], ENT_QUOTES, "UTF-8");
 
