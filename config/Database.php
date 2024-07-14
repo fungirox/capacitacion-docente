7d3557b6ffcbf6384473498a5a0f998de921f54b
Diego Rascón <jose.d.rascon@gmail.com>
2024-07-14 16:40:16 -0700

    Se implementaron métodos para las requests y manejar errores

--- config/Database.php
+++ config/Database.php
@@ -1,6 +1,7 @@
 <?php
 class Database {
     public $connection;
+    public $statement;
 
     public function __construct() {
         require "env.php";
@@ -13,9 +14,27 @@ class Database {
     }
 
     public function query($query, $params = []) {
-        $statement = $this->connection->prepare($query);
-        $statement->execute($params);
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
 
-        return $statement;
+        return $this;
+    }
+
+    public function getAll() {
+        return $this->statement->fetchAll();
+    }
+
+    public function get() {
+        return $this->statement->fetch();
+    }
+
+    public function getOrFail() {
+        $result = $this->get();
+
+        if(!$result) {
+            abort();
+        }
+
+        return $result;
     }
 }
