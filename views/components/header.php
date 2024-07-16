f203fa2e78308e43561ad196a66eed9061a8da08
Diego Rascón <jose.d.rascon@gmail.com>
2024-07-16 14:25:46 -0700

    Se corrigió la ruta del ícono de ITESCA

--- views/components/header.php
+++ views/components/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($title) ? "$title - Capacitación Docente ITESCA" : "Página no encontrada - Capacitación Docente ITESCA" ?></title>
-    <link rel="icon" href="__DIR__../../assets/images/icono-itesca.png">
+    <link rel="icon" href="../../assets/images/icono-itesca.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
