4364e2f59e52d0ff53d41fa89a791a5975838c55
Diego Rascón <jose.d.rascon@gmail.com>
2024-07-16 13:53:46 -0700

    Se bajó la opacidad del separador y se corrigió el padding

--- views/components/styledFooter.php
+++ views/components/styledFooter.php
@@ -1,5 +1,7 @@
-<hr class="container">
-<footer class="py-4">
-    <p class="text-center text-body-secondary">© Copyright 2024 ITESCA - Todos los Derechos Reservados</p>
-</footer>
+<div class="container">
+    <hr style="opacity: 0.1;" />
+    <footer class="py-4">
+        <p class="text-center text-body-secondary">© Copyright 2024 ITESCA - Todos los Derechos Reservados</p>
+    </footer>
+</div>
 <?php require __DIR__ . "/footer.php"; ?>
