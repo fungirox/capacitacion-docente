962dd5f11f2a5f09c8c54100c4c0a98988e6f431
Diego Rascón <jose.d.rascon@gmail.com>
2024-07-30 15:51:13 -0700

    Se agregó el query para agregar carreras

--- controllers/admin/carreras/carreras-nuevo.php
+++ controllers/admin/carreras/carreras-nuevo.php
@@ -5,7 +5,10 @@ $db = new Database();
 $title = "Nueva Área";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        
+    $db->query(
+        "INSERT INTO tblCarrera (CARRERA_Nombre, CARRERA_Siglas) VALUES (?, ?)",
+        [$_POST["nombre"], $_POST["siglas"]]
+    );
 }
 
 require "views/admin/carreras/carreras-nuevo.view.php";
