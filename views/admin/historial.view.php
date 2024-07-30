7825a356b6d515158e16d9c5026e3c8854fbf7a3
Diego Rascón <jose.d.rascon@gmail.com>
2024-07-29 21:47:42 -0700

    Vista de historial

---
+++ views/admin/historial.view.php
@@ -0,0 +1,5 @@
+<?php require $_SERVER['DOCUMENT_ROOT'] . "/views/components/styledHeader.php"; ?>
+<main role="main" class="container py-4" style="margin-top: 56px">
+    <h1>Historial de Cursos</h1>
+</main>
+<?php require $_SERVER['DOCUMENT_ROOT'] . "/views/components/styledFooter.php"; ?>
