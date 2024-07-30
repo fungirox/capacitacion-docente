675f3cf8a0d91c95876e807547cc34659fda49d7
Diego Rascón <jose.d.rascon@gmail.com>
2024-07-30 15:52:33 -0700

    Se agregaron los nombres para los inputs del formulario

--- views/admin/carreras/carreras-nuevo.view.php
+++ views/admin/carreras/carreras-nuevo.view.php
@@ -1,14 +1,14 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . "/views/components/styledHeader.php"; ?>
 <main role="main" class="container py-4" style="margin-top: 56px">
     <h1>Carreras</h1>
-    <form class="row py-4 g-3">
+    <form class="row py-4 g-3" method="POST">
         <div class="col-md-8">
-            <label for="inputEmail4" class="form-label">Nombre de la carrera</label>
-            <input type="email" class="form-control" id="inputEmail4">
+            <label for="nombre" class="form-label">Nombre de la carrera</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" required>
         </div>
         <div class="col-md-4">
-            <label for="inputPassword4" class="form-label">Abreviación</label>
-            <input type="password" class="form-control" id="inputPassword4">
+            <label for="siglas" class="form-label">Siglas</label>
+            <input type="text" class="form-control" id="siglas" name="siglas" required>
             <div class="form-text" id="basic-addon4">Usualmente de 3 a 4 letras.</div>
         </div>
         <div class="col-12 col-md-auto d-grid">
