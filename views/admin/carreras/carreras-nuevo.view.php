6eafa1804d3f69753ac319cc8eb4659ef8017378
Diego Rascón <jose.d.rascon@gmail.com>
2024-08-18 00:21:32 -0700

    Se implementó la validación del formulario

--- views/admin/carreras/carreras-nuevo.view.php
+++ views/admin/carreras/carreras-nuevo.view.php
@@ -1,15 +1,28 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . "/views/components/styledHeader.php"; ?>
+<script defer>
+    $(document).ready(function() {
+        $('input').on('input', function() {
+            $(this).removeClass('is-invalid');
+        });
+    });
+</script>
 <main role="main" class="container py-4" style="margin-top: 56px">
     <h1>Carreras</h1>
     <form class="row py-4 g-3" method="POST">
         <div class="col-md-8">
             <label for="nombre" class="form-label">Nombre de la carrera</label>
-            <input type="text" class="form-control" id="nombre" name="nombre" required>
+            <input type="text" class="form-control <?= isset($errors['nombre']) ? 'is-invalid' : '' ?>" id="nombre" name="nombre" value="<?= isset($_POST['nombre']) ? htmlspecialchars($_POST['nombre']) : '' ?>">
+            <div class="invalid-feedback">
+                <?= $errors['nombre'] ?>
+            </div>
         </div>
         <div class="col-md-4">
             <label for="siglas" class="form-label">Siglas</label>
-            <input type="text" class="form-control" id="siglas" name="siglas" required>
-            <div class="form-text" id="basic-addon4">Usualmente de 3 a 4 letras.</div>
+            <input type="text" class="form-control <?= isset($errors['siglas']) ? 'is-invalid' : '' ?>" id="siglas" name="siglas" value="<?= isset($_POST['siglas']) ? htmlspecialchars($_POST['siglas']) : '' ?>">
+            <div class="form-text">Usualmente de 3 a 4 letras.</div>
+            <div class="invalid-feedback">
+                <?= $errors['siglas'] ?>
+            </div>
         </div>
         <div class="col-12 col-md-auto d-grid">
             <button type="submit" class="btn btn-primary">Crear</button>
