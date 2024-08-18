65776ee5c2c31646a688df54c6a19cd1cc208366
Diego Rascón <jose.d.rascon@gmail.com>
2024-08-18 00:20:58 -0700

    Se implementó el uso de la clase validación

--- controllers/admin/carreras/carreras-nuevo.php
+++ controllers/admin/carreras/carreras-nuevo.php
@@ -1,14 +1,32 @@
 <?php
 
+require 'Validator.php';
+
 $db = new Database();
 
 $title = "Nueva Área";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $db->query(
-        "INSERT INTO tblCarrera (CARRERA_Nombre, CARRERA_Siglas) VALUES (?, ?)",
-        [$_POST["nombre"], $_POST["siglas"]]
-    );
+    $errors = [];
+
+    $siglas = trim(strtoupper($_POST["siglas"]));
+
+    $validator = new Validator();
+
+    if (!$validator->string($_POST["nombre"], 1, 1000)) {
+        $errors['nombre'] = "Favor de introducir el nombre de la carrera.";
+    }
+
+    if (!$validator->string($_POST["nombre"], 1, 1000)) {
+        $errors['nombre'] = "Favor de introducir el nombre de la carrera.";
+    }
+
+    if (empty($errors)) {
+        $db->query(
+            "INSERT INTO tblCarrera (CARRERA_Nombre, CARRERA_Siglas) VALUES (?, ?)",
+            [$nombre, $siglas]
+        );
+    }
 }
 
 require "views/admin/carreras/carreras-nuevo.view.php";
