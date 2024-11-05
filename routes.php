52cfbbba0cdfadaf7fa4dc6ec9e8a88e708e3f9b
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-04 20:45:17 -0700

    Se cambió el string de admin

--- routes.php
+++ routes.php
@@ -1,103 +1,52 @@
 <?php
 
-// return [
-//     # Auth
-
-//     "/login" => "views/login.view.php",
-//     "/authenticate" => "controllers/auth/authenticate.php",
-//     "/logout" => "controllers/auth/logOut.php",
-//     "/" => "controllers/oferta.php",
-
-//     # Administradores
-
-//     # Cursos
-//     "/admin/cursos" => "controllers/admin/cursos/cursos.php",
-//     "/admin/cursos/nuevo" => "controllers/admin/cursos/curso-nuevo.php",
-//     "/admin/curso" => "controllers/admin/cursos/curso.php",
-
-//     # Historial
-//     "/admin/historial" => "controllers/admin/historial/historial.php",
-
-//     # Docentes
-//     "/admin/docentes" => "controllers/admin/docentes/docentes.php",
-//     "/admin/docentes/nuevo" => "controllers/admin/docentes/docentes-nuevo.php",
-
-//     # Instructores
-//     "/admin/instructores" => "controllers/admin/instructores.php",
-
-//     # Documentación
-//     "/admin/reportes" => "controllers/admin/reportes.php",
-//     "/admin/formatos" => "controllers/admin/formatos.php",
-
-//     # Académico
-//     # Áreas
-//     "/admin/areas" => "controllers/admin/areas/areas.php",
-//     "/admin/areas/nuevo" => "controllers/admin/areas/areas.php",
-
-//     # Carreras
-//     "/admin/carreras" => "controllers/admin/carreras/carreras.php",
-//     "/admin/carreras/nuevo" => "controllers/admin/carreras/carreras-nuevo.php",
-
-//     # Usuarios normales
-
-//     # Oferta
-//     "/oferta" => "controllers/oferta.php",
-//     "/inscritos" => "controllers/inscritos.php",
-//     "/instruyendo" => "controllers/instruyendo.php",
-
-//     "/horario" => "controllers/horario.php",
-//     "/historial" => "controllers/historial.php",
-
-//     # Encuesta TODO
-//     "/evaluarCurso" => "views/encuestas/encuesta-F04PSA19.php",
-
-//     # Generar Reporte
-//     "/generarReporte" => "controllers/generarReporte.php",
-// ];
-
 # Auth
 
 $router->get("/login", "controllers/auth/login.php")->only("guest");
 
 $router->post("/login", "controllers/auth/authenticate.php")->only("guest");
 
-$router->delete("/logout", "controllers/auth/logout.php")->only("admin");
+$router->delete("/logout", "controllers/auth/logout.php")->only("admins");
+
+# Docentes
+
+$router->get("/oferta", "controllers/docentes/oferta/index.php")->only("docente");
 
 # Administradores
 
 # Cursos
 
-$router->get("/admin/cursos", "controllers/admin/cursos/index.php")->only("admin");
-$router->get("/admin/curso", "controllers/admin/cursos/show.php")->only("admin");
-$router->get("/admin/cursos/nuevo", "controllers/admin/cursos/create.php")->only("admin");
+$router->get("/admin/cursos", "controllers/admin/cursos/index.php")->only("admins");
+$router->get("/admin/curso", "controllers/admin/cursos/show.php")->only("admins");
+$router->get("/admin/cursos/nuevo", "controllers/admin/cursos/create.php")->only("admins");
 
 # Usuarios
 
-$router->get("/admin/usuarios", "controllers/admin/usuarios/index.php")->only("admin");
-$router->get("/admin/usuarios/nuevo", "controllers/admin/usuarios/create.php")->only("admin");
+$router->get("/admin/usuarios", "controllers/admin/usuarios/index.php")->only("admins");
+$router->get("/admin/usuarios/nuevo", "controllers/admin/usuarios/create.php")->only("admins");
 
-$router->post("/admin/usuarios", "controllers/admin/usuarios/store.php")->only("admin");
+$router->post("/admin/usuarios", "controllers/admin/usuarios/store.php")->only("admins");
 
 # Áreas
 
-$router->get("/admin/areas", "controllers/admin/areas/index.php")->only("admin");
-$router->get("/admin/areas/editar", "controllers/admin/areas/edit.php")->only("admin");
-$router->get("/admin/areas/nuevo", "controllers/admin/areas/create.php")->only("admin");
+$router->get("/admin/areas", "controllers/admin/areas/index.php")->only("admins");
+$router->get("/admin/areas/editar", "controllers/admin/areas/edit.php")->only("admins");
+$router->get("/admin/areas/nuevo", "controllers/admin/areas/create.php")->only("admins");
 
-$router->post("/admin/areas", "controllers/admin/areas/store.php")->only("admin");
+$router->post("/admin/areas", "controllers/admin/areas/store.php")->only("admins");
 
-$router->delete("/admin/areas", "controllers/admin/areas/destroy.php")->only("admin");
+$router->delete("/admin/areas", "controllers/admin/areas/destroy.php")->only("admins");
 
-$router->patch("/admin/areas", "controllers/admin/areas/update.php")->only("admin");
+$router->patch("/admin/areas", "controllers/admin/areas/update.php")->only("admins");
 
 # Carreras
 
-$router->get("/admin/carreras", "controllers/admin/carreras/index.php")->only("admin");
-$router->get("/admin/carreras/editar", "controllers/admin/carreras/edit.php")->only("admin");
-$router->get("/admin/carreras/nuevo", "controllers/admin/carreras/create.php")->only("admin");
+$router->get("/admin/carreras", "controllers/admin/carreras/index.php")->only("admins");
+$router->get("/admin/carreras/editar", "controllers/admin/carreras/edit.php")->only("admins");
+$router->get("/admin/carreras/nuevo", "controllers/admin/carreras/create.php")->only("admins");
 
-$router->post("/admin/carreras", "controllers/admin/carreras/store.php")->only("admin");
+$router->post("/admin/carreras", "controllers/admin/carreras/store.php")->only("admins");
 
-$router->delete("/admin/carreras", "controllers/admin/carreras/destroy.php")->only("admin");
+$router->delete("/admin/carreras", "controllers/admin/carreras/destroy.php")->only("admins");
 
-$router->patch("/admin/carreras", "controllers/admin/carreras/update.php")->only("admin");
+$router->patch("/admin/carreras", "controllers/admin/carreras/update.php")->only("admins");
