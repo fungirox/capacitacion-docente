ef667bd266e152778d4979a437f686f33d55cc09
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-10 12:59:36 -0700

    Nueva clase Roles

---
+++ Core/Roles/Roles.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Core\Roles;
+
+class Roles {
+    public const GUEST = "guest";
+    public const ADMIN = "admin";
+    public const DOCENTE = "docente";
+    public const INSTRUCTOR = "instructor";
+    public const DOCENTE_INSTRUCTOR = "docenteInstructor";
+    public const ANY = "any";
+}
