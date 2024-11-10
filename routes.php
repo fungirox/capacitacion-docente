608012c5f0037fa5a01fb588e2ffab0db8162bd7
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-10 12:59:01 -0700

    Se usa la nueva clase de Roles

--- routes.php
+++ routes.php
@@ -1,22 +1,31 @@
 <?php
 
+use Core\Roles\Roles;
+
+const GUEST = Roles::GUEST;
+const ADMIN = Roles::ADMIN;
+const DOCENTE = Roles::DOCENTE;
+const DOCENTE_INSTRUCTOR = Roles::DOCENTE_INSTRUCTOR;
+const INSTRUCTOR = Roles::INSTRUCTOR;
+const ANY = Roles::ANY;
+
 ## Auth
 
-$router->get("/login", "controllers/auth/login.php")->only("guest");
+$router->get("/login", "controllers/auth/login.php")->only(GUEST);
 
-$router->post("/login", "controllers/auth/authenticate.php")->only("guest");
+$router->post("/login", "controllers/auth/authenticate.php")->only(GUEST);
 
-$router->delete("/logout", "controllers/auth/logout.php")->only("admin");
+$router->delete("/logout", "controllers/auth/logout.php")->only(ANY);
 
 ## Docente
 
 # Inscritos
 
-$router->get("/inscritos", "controllers/docente/inscritos/index.php")->only("docente");
+$router->get("/inscritos", "controllers/docente/inscritos/index.php")->only(DOCENTE);
 
 # Oferta
 
-$router->get("/oferta", "controllers/docente/oferta/index.php")->only("docente");
+$router->get("/oferta", "controllers/docente/oferta/index.php")->only(DOCENTE);
 
 ## Inscritos
 
@@ -26,43 +35,43 @@ $router->get("/instruyendo", "");
 
 # Cursos
 
-$router->get("/admin/cursos", "controllers/admin/cursos/index.php")->only("admin");
-$router->get("/admin/curso", "controllers/admin/cursos/show.php")->only("admin");
-$router->get("/admin/cursos/nuevo", "controllers/admin/cursos/create.php")->only("admin");
+$router->get("/admin/cursos", "controllers/admin/cursos/index.php")->only(ADMIN);
+$router->get("/admin/curso", "controllers/admin/cursos/show.php")->only(ADMIN);
+$router->get("/admin/cursos/nuevo", "controllers/admin/cursos/create.php")->only(ADMIN);
 
-$router->post("/admin/cursos", "controllers/admin/cursos/store.php")->only("admin");
+$router->post("/admin/cursos", "controllers/admin/cursos/store.php")->only(ADMIN);
 
-$router->delete("/admin/cursos", "controllers/admin/cursos/destroy.php")->only("admin");
+$router->delete("/admin/cursos", "controllers/admin/cursos/destroy.php")->only(ADMIN);
 
-$router->patch("/admin/cursos", "controllers/admin/cursos/update.php")->only("admin");
+$router->patch("/admin/cursos", "controllers/admin/cursos/update.php")->only(ADMIN);
 
 # Usuarios
 
-$router->get("/admin/usuarios", "controllers/admin/usuarios/index.php")->only("admin");
-$router->get("/admin/usuarios/nuevo", "controllers/admin/usuarios/create.php")->only("admin");
+$router->get("/admin/usuarios", "controllers/admin/usuarios/index.php")->only(ADMIN);
+$router->get("/admin/usuarios/nuevo", "controllers/admin/usuarios/create.php")->only(ADMIN);
 
-$router->post("/admin/usuarios", "controllers/admin/usuarios/store.php")->only("admin");
+$router->post("/admin/usuarios", "controllers/admin/usuarios/store.php")->only(ADMIN);
 
 # Áreas
 
-$router->get("/admin/areas", "controllers/admin/areas/index.php")->only("admin");
-$router->get("/admin/areas/editar", "controllers/admin/areas/edit.php")->only("admin");
-$router->get("/admin/areas/nuevo", "controllers/admin/areas/create.php")->only("admin");
+$router->get("/admin/areas", "controllers/admin/areas/index.php")->only(ADMIN);
+$router->get("/admin/areas/editar", "controllers/admin/areas/edit.php")->only(ADMIN);
+$router->get("/admin/areas/nuevo", "controllers/admin/areas/create.php")->only(ADMIN);
 
-$router->post("/admin/areas", "controllers/admin/areas/store.php")->only("admin");
+$router->post("/admin/areas", "controllers/admin/areas/store.php")->only(ADMIN);
 
-$router->delete("/admin/areas", "controllers/admin/areas/destroy.php")->only("admin");
+$router->delete("/admin/areas", "controllers/admin/areas/destroy.php")->only(ADMIN);
 
-$router->patch("/admin/areas", "controllers/admin/areas/update.php")->only("admin");
+$router->patch("/admin/areas", "controllers/admin/areas/update.php")->only(ADMIN);
 
 # Carreras
 
-$router->get("/admin/carreras", "controllers/admin/carreras/index.php")->only("admin");
-$router->get("/admin/carreras/editar", "controllers/admin/carreras/edit.php")->only("admin");
-$router->get("/admin/carreras/nuevo", "controllers/admin/carreras/create.php")->only("admin");
+$router->get("/admin/carreras", "controllers/admin/carreras/index.php")->only(ADMIN);
+$router->get("/admin/carreras/editar", "controllers/admin/carreras/edit.php")->only(ADMIN);
+$router->get("/admin/carreras/nuevo", "controllers/admin/carreras/create.php")->only(ADMIN);
 
-$router->post("/admin/carreras", "controllers/admin/carreras/store.php")->only("admin");
+$router->post("/admin/carreras", "controllers/admin/carreras/store.php")->only(ADMIN);
 
-$router->delete("/admin/carreras", "controllers/admin/carreras/destroy.php")->only("admin");
+$router->delete("/admin/carreras", "controllers/admin/carreras/destroy.php")->only(ADMIN);
 
-$router->patch("/admin/carreras", "controllers/admin/carreras/update.php")->only("admin");
+$router->patch("/admin/carreras", "controllers/admin/carreras/update.php")->only(ADMIN);
