6f3a34007359307ed1412e9b52fa641ee2fed056
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-10 19:11:40 -0700

    Se corrigió la condición del middleware

--- Core/Middleware/DocenteOrInstructor.php
+++ Core/Middleware/DocenteOrInstructor.php
@@ -9,7 +9,7 @@ class DocenteOrInstructor extends MiddlewareTemplate {
     public function handle() {
         if (!$this->isAuthenticated()) {
             $this->redirect("/login");
-        } elseif ($_SESSION["user"]["rol"] !== Roles::DOCENTE || $_SESSION["user"]["rol"] !== Roles::INSTRUCTOR || $_SESSION["user"]["rol"] !== Roles::DOCENTE_AND_INSTRUCTOR) {
+        } elseif ($_SESSION["user"]["rol"] !== Roles::DOCENTE && $_SESSION["user"]["rol"] !== Roles::INSTRUCTOR && $_SESSION["user"]["rol"] !== Roles::DOCENTE_AND_INSTRUCTOR) {
             $this->redirect("/");
         }
     }
