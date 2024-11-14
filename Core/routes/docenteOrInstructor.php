4a0d40a52d7db09a7de32d2e84d2fb01791e0227
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-13 19:06:29 -0700

    Se agregó la ruta de historial

--- Core/routes/docenteOrInstructor.php
+++ Core/routes/docenteOrInstructor.php
@@ -3,3 +3,7 @@
 use Core\Roles\Roles;
 
 const DOCENTE_OR_INSTRUCTOR = Roles::DOCENTE_OR_INSTRUCTOR;
+
+# Historial
+
+$router->get("/historial", "controllers/docenteOrInstructor/historial/index.php")->only(DOCENTE_OR_INSTRUCTOR);
