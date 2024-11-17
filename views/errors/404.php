f92a503d730cf92c1ec25ca134583f7a3b62f547
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-17 14:33:14 -0700

    Se actualizó el uso de view

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,7 +1,7 @@
-<?php require view("components/header.php"); ?>
+<?php view("components/header.php"); ?>
 <main role="main" class="container text-center pt-4">
     <h1>Error 404</h1>
     <p>No se encontró la página deseada</p>
     <a href="/">Volver al inicio</a>
 </main>
-<?php require view("components/footer.php"); ?>
+<?php view("components/footer.php"); ?>
