ec1d4d71eee799ffe212f02719cb2b4acf0602ea
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-17 13:41:02 -0700

    Se corrigió el título de la vista

--- views/admin/carreras/index.view.php
+++ views/admin/carreras/index.view.php
@@ -10,88 +10,88 @@
     });
 </script>
 <main role="main" class="container py-4" style="margin-top: 56px">
-    <h1<?= $title ?>< /h1>
-        <div class="row row-cols-auto justify-content-end py-4 g-2">
-            <div class="order-2 order-md-1 col-12 col-md-4">
-                <div class="input-group flex-nowrap">
-                    <span class="input-group-text" id="addon-wrapping">
-                        <i class="bi bi-search"></i>
-                    </span>
-                    <input type="text" id="search-docente" class="form-control" placeholder="Buscar carrera..." aria-label="BuscarDocente" aria-describedby="buscar-docente">
-                </div>
-            </div>
-            <div class="order-1 order-md-2 d-grid col-12 col-md-auto">
-                <a href="carreras/nuevo" type="button" class="btn btn-primary">
-                    <i class="bi bi-plus-lg"></i>
-                    <span>Nueva carrera</span>
-                </a>
+    <h1><?= $title ?></h1>
+    <div class="row row-cols-auto justify-content-end py-4 g-2">
+        <div class="order-2 order-md-1 col-12 col-md-4">
+            <div class="input-group flex-nowrap">
+                <span class="input-group-text" id="addon-wrapping">
+                    <i class="bi bi-search"></i>
+                </span>
+                <input type="text" id="search-docente" class="form-control" placeholder="Buscar carrera..." aria-label="BuscarDocente" aria-describedby="buscar-docente">
             </div>
         </div>
-        <div class="table-responsive">
-            <table class="table table-striped table-hover align-middle">
-                <thead>
+        <div class="order-1 order-md-2 d-grid col-12 col-md-auto">
+            <a href="carreras/nuevo" type="button" class="btn btn-primary">
+                <i class="bi bi-plus-lg"></i>
+                <span>Nueva carrera</span>
+            </a>
+        </div>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-striped table-hover align-middle">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Siglas</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($allCareers as $career) : ?>
+                    <?php
+                    $id = $career['CARRERAID'];
+                    $name = htmlspecialchars($career['CARRERA_Nombre']);
+                    $siglas = htmlspecialchars($career['CARRERA_Siglas']);
+                    ?>
                     <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Siglas</th>
-                        <th scope="col"></th>
+                        <td><?= $name ?></td>
+                        <td><?= $siglas ?></td>
+                        <td class="text-end">
+                            <div class="dropdown">
+                                <button class="btn btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-three-dots-vertical"></i>
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li>
+                                        <a class="dropdown-item" href="/admin/carreras/editar?id=<?= $id ?>">
+                                            <i class="bi bi-pencil"></i>
+                                            <span class="ms-2">Editar</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <button class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteModal<?= $id ?>">
+                                            <i class="bi bi-trash"></i>
+                                            <span class="ms-2">Eliminar</span>
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($allCareers as $career) : ?>
-                        <?php
-                        $id = $career['CARRERAID'];
-                        $name = htmlspecialchars($career['CARRERA_Nombre']);
-                        $acronym = htmlspecialchars($career['CARRERA_Siglas']);
-                        ?>
-                        <tr>
-                            <td><?= $name ?></td>
-                            <td><?= $acronym ?></td>
-                            <td class="text-end">
-                                <div class="dropdown">
-                                    <button class="btn btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="bi bi-three-dots-vertical"></i>
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-end">
-                                        <li>
-                                            <a class="dropdown-item" href="/admin/carreras/editar?id=<?= $id ?>">
-                                                <i class="bi bi-pencil"></i>
-                                                <span class="ms-2">Editar</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <button class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteModal<?= $id ?>">
-                                                <i class="bi bi-trash"></i>
-                                                <span class="ms-2">Eliminar</span>
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
-                        <form method="POST">
-                            <div class="modal fade" id="deleteModal<?= $id ?>" tabindex="-1" aria-labelledby="deleteModalLabel<?= $id ?>" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="deleteModalLabel<?= $id ?>">Eliminar Carrera</h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>¿Seguro de que desea eliminar esta carrera?</p>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <input type="hidden" name="_method" value="DELETE" />
-                                            <input type="hidden" name="id" value="<?= $id ?>" />
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                                        </div>
+                    <form method="POST">
+                        <div class="modal fade" id="deleteModal<?= $id ?>" tabindex="-1" aria-labelledby="deleteModalLabel<?= $id ?>" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="deleteModalLabel<?= $id ?>">Eliminar Carrera</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>¿Seguro de que desea eliminar la carrera <?= $siglas ?>?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <input type="hidden" name="_method" value="DELETE" />
+                                        <input type="hidden" name="id" value="<?= $id ?>" />
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                        <button type="submit" class="btn btn-danger">Eliminar</button>
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
+                        </div>
+                    </form>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </main>
 <?php view("components/styledFooter.php"); ?>
