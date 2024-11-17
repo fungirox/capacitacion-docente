4212df1d4d82af529a3733df3a6208319e29e5ad
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-17 00:17:02 -0700

    Se actualizaron los nombres de los controladores

--- Core/routes/auth.php
+++ Core/routes/auth.php
@@ -5,8 +5,8 @@ use Core\Roles\Roles;
 const GUEST = Roles::GUEST;
 const ANY = Roles::ANY;
 
-$router->get("/login", "controllers/auth/login.php")->only(GUEST);
+$router->get("/login", "controllers/auth/index.php")->only(GUEST);
 
-$router->post("/login", "controllers/auth/authenticate.php")->only(GUEST);
+$router->post("/login", "controllers/auth/store.php")->only(GUEST);
 
-$router->delete("/logout", "controllers/auth/logout.php")->only(ANY);
+$router->delete("/logout", "controllers/auth/delete.php")->only(ANY);
