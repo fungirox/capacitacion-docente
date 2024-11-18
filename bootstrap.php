fef6d7764fa429fdfaac35a94e1fb2aa83f29df1
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-17 22:49:12 -0700

    Se agregó el binding para CarreraRepository

--- bootstrap.php
+++ bootstrap.php
@@ -3,11 +3,16 @@
 use Core\App;
 use Core\Container;
 use Core\Database;
+use Core\Repositories\CarreraRepository;
 
 $container = new Container();
 
 $container->bind("Core\Database", function () {
     return new Database();
+});
+
+$container->bind("Core\Repositories\CarreraRepository", function () {
+    return new CarreraRepository(App::resolve(Database::class));
 });
 
 App::setContainer($container);
