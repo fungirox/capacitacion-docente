0b973116fe8901e906bc0fbbadde29571a29c130
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-17 22:48:46 -0700

    Clase con queries para la tabla carrera

---
+++ Core/Repositories/CarreraRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Core\Repositories;
+
+use Core\Repositories\RepositoryTemplate;
+
+class CarreraRepository extends RepositoryTemplate {
+
+    public function getAll() {
+        return $this->query("SELECT * FROM tblCarrera")->getAll();
+    }
+
+    public function getById($id) {
+        return $this->query("SELECT * FROM tblCarrera WHERE CARRERAID = ?", [$id])->getOrFail();
+    }
+
+    public function create($values) {
+        $this->query(
+            "INSERT INTO tblCarrera (CARRERA_Nombre, CARRERA_Siglas) VALUES (?, ?)",
+            [$values["nombre"], $values["siglas"]]
+        );
+    }
+
+    public function update($values) {
+        $this->query(
+            "UPDATE tblCarrera SET CARRERA_Nombre = ?, CARRERA_Siglas = ? WHERE CARRERAID = ?",
+            [$values["nombre"], $values["siglas"], $values["id"]]
+        );
+    }
+
+    public function delete($id) {
+        $this->query("DELETE FROM tblCarrera WHERE CARRERAID = ?", [$id]);
+    }
+}
