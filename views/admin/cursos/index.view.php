9353b7ad3f3a000902bb578f3878b86939a66e8a
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-24 23:31:21 -0700

    Se agregaron colores de badges, ajustes de espaciado y se muestra la duración del curso

--- views/admin/cursos/index.view.php
+++ views/admin/cursos/index.view.php
@@ -37,43 +37,45 @@
     <div class="my-2 row row-cols-1 row-cols-md-3 g-4">
         <?php foreach ($allCourses as $course) : ?>
             <?php
-            $id = $course["CURSOID"];
-            $nombre = htmlspecialchars($course["CURSO_Nombre"], ENT_QUOTES, "UTF-8");
+            $id = htmlspecialchars($course["CURSOID"]);
+            $nombre = htmlspecialchars($course["CURSO_Nombre"]);
+            $instructorNombre = htmlspecialchars($course["instructor_nombre"]);
+            $duracion = htmlspecialchars($course["CURSO_Total_Horas"] . " horas");
 
-            $instructorQuery = $db->query("SELECT tblUsuario.USER_Nombre, tblUsuario.USER_Apellido  
-            FROM tblUsuario 
-            INNER JOIN tblInstructor ON tblUsuario.USERID = tblInstructor.USERID 
-            INNER JOIN tblCursoInstructor ON tblCursoInstructor.INSTRUCTORID = tblInstructor.INSTRUCTORID
-            INNER JOIN tblCurso ON tblCurso.CURSOID = tblCursoInstructor.CURSOID
-            WHERE tblCurso.CURSOID = ?", [$id])->get();
+            $tipo = htmlspecialchars($course["CURSO_Tipo"]);
+            $tipoColor = $tipo === "curso" ? "text-bg-primary" : "text-bg-warning";
 
-            if ($instructorQuery) {
-                $nombreInstructor = htmlspecialchars($instructorQuery['USER_Nombre'], ENT_QUOTES, "UTF-8");
-                $apellidoInstructor = htmlspecialchars($instructorQuery['USER_Apellido'], ENT_QUOTES, "UTF-8");
-            } else {
-                $nombreInstructor = "Instructor";
-                $apellidoInstructor = "no encontrado";
-            }
-
-            $tipo = htmlspecialchars($course["CURSO_Tipo"], ENT_QUOTES, "UTF-8");
-
-            $modalidad = htmlspecialchars($course["CURSO_Modalidad"], ENT_QUOTES, "UTF-8");
-
+            $modalidad = htmlspecialchars($course["CURSO_Modalidad"]);
+            $modalidadColor = match ($modalidad) {
+                "virtual" => "text-bg-success",
+                "presencial" => "text-bg-danger",
+                "hibrido" => "text-bg-info"
+            };
+            $modalidad = $modalidad === "hibrido" ? "híbrido" : $modalidad;
             ?>
             <div class="col">
-                <div class="card h-100 d-flex flex-column">
-                    <a class="link-dark link-offset-2 link-underline link-underline-opacity-0 d-flex flex-column h-100" href="curso?id=<?= $id ?>">
-                        <img src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="150px" style="object-fit:cover;">
-                        <div class="card-body flex-grow-1 d-flex flex-column">
-                            <h5 class="card-title"><?= $nombre ?></h5>
-                            <p class="card-text"><?= $nombreInstructor ?> <?= $apellidoInstructor ?> </p>
-                        </div>
-
-                        <div class="card-footer mt-auto">
-                            <span class="list-inline-item text-capitalize badge text-bg-primary"> <?= $tipo ?> </span>
-                            <p class="list-inline-item text-capitalize badge text-bg-primary"> <?= $modalidad ?></p>
-                        </div>
+                <div class="card h-100">
+                    <a href="/admin/curso?id=<?= $id ?>">
+                        <img
+                            src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            class="card-img-top"
+                            height="160px"
+                            style="object-fit: cover"
+                            alt="Portada del curso">
                     </a>
+                    <div class="card-body flex-grow-1 d-flex flex-column">
+                        <a class="h5 card-title link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="/admin/cursos/curso?id=<?= $id ?>">
+                            <?= $nombre ?>
+                        </a>
+                        <span class="card-text"><?= $instructorNombre ?></span>
+                    </div>
+                    <div class="card-footer px-4 gap-2 d-flex justify-content-between align-items-center">
+                        <div class="row gap-2">
+                            <span class="col-auto text-capitalize badge rounded-pill <?= $tipoColor ?>"> <?= $tipo ?> </span>
+                            <span class="col-auto text-capitalize badge rounded-pill <?= $modalidadColor ?>"> <?= $modalidad ?></span>
+                        </div>
+                        <span class="text-secondary text-end"><?= $duracion ?></span>
+                    </div>
                 </div>
             </div>
         <?php endforeach; ?>
