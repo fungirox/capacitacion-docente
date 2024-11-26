f03c45d5b91c87a681020e1b122c421da97de3d9
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-25 18:45:50 -0700

    Se actualizó el query para obtener las areas

--- Core/Repositories/CursoRepository.php
+++ Core/Repositories/CursoRepository.php
@@ -9,19 +9,31 @@ class CursoRepository extends RepositoryTemplate {
             "SELECT
                 curso.CURSOID,
                 curso.CURSO_Nombre,
+                curso.CURSO_Tipo + ' ' + curso.CURSO_Modalidad as tipo,
+                curso.CURSO_Activo,
+                STRING_AGG(area.AREA_Siglas, ',') AS areas,
+                usuario.USER_Nombre + ' ' + usuario.USER_Apellido AS instructor_nombre
+            FROM
+                tblCurso AS curso
+            LEFT JOIN
+                tblCursoInstructor AS cursoInstructor ON curso.CURSOID = cursoInstructor.CURSOID
+            LEFT JOIN
+                tblInstructor AS instructor ON instructor.INSTRUCTORID = cursoInstructor.INSTRUCTORID
+            LEFT JOIN
+                tblUsuario AS usuario ON usuario.USERID = instructor.USERID
+            LEFT JOIN
+                tblCursoArea AS cursoArea ON cursoArea.CURSOID = curso.CURSOID
+            LEFT JOIN
+                tblArea AS area ON area.AREAID = cursoArea.AREAID
+            GROUP BY
+                curso.CURSOID,
+                curso.CURSO_Nombre,
                 curso.CURSO_Tipo,
                 curso.CURSO_Modalidad,
                 curso.CURSO_Total_Horas,
                 curso.CURSO_Activo,
-                usuario.USER_Nombre + ' ' + usuario.USER_Apellido as instructor_nombre
-            FROM
-                tblCurso as curso
-            LEFT JOIN
-                tblCursoInstructor as cursoInstructor ON curso.CURSOID = cursoInstructor.CURSOID
-            LEFT JOIN
-                tblInstructor as instructor ON instructor.INSTRUCTORID = cursoInstructor.INSTRUCTORID
-            LEFT JOIN
-                tblUsuario as usuario ON usuario.USERID = instructor.USERID;"
+                usuario.USER_Nombre,
+                usuario.USER_Apellido"
         )->getAll();
     }
 }
