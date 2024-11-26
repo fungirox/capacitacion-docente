0f36f6803196a4b0306efcbc6770c422b2918aa1
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-25 18:46:04 -0700

    Se obtiene una lista de las áreas de cada  cursp

--- Http/controllers/admin/cursos/index.php
+++ Http/controllers/admin/cursos/index.php
@@ -3,7 +3,14 @@
 use Core\App;
 use Core\Repositories\CursoRepository;
 
+$allCursos = App::resolve(CursoRepository::class)->getAll();
+
+foreach ($allCursos as $key => $curso) {
+    $areas = explode(",", $curso["areas"]);
+    $allCursos[$key]["areas"] = $areas;
+}
+
 return view("admin/cursos/index.view.php", [
     "title" => "Cursos",
-    "allCourses" => App::resolve(CursoRepository::class)->getAll()
+    "allCourses" => $allCursos
 ]);
