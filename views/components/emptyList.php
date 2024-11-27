f3bfe309318b9d4c7345e606f203836a8098cd81
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-26 21:31:48 -0700

    Componente para listas vacías

---
+++ views/components/emptyList.php
@@ -0,0 +1,4 @@
+<div class="d-flex flex-column align-items-center py-5 gap-2">
+    <i class="bi bi-emoji-frown fs-1 text-secondary"></i>
+    <span class="text-secondary">No se encontraron registros...</span>
+</div>
