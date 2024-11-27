423c51f52640a5683072ce01acf450a5e2a160e2
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-26 18:12:03 -0700

    Se agregó la función para obtener un curso por ID

--- Core/Repositories/CursoRepository.php
+++ Core/Repositories/CursoRepository.php
@@ -36,4 +36,46 @@ class CursoRepository extends RepositoryTemplate {
                 usuario.USER_Apellido"
         )->getAll();
     }
+
+    public function getById($cursoId) {
+        return $this->query(
+            "SELECT
+                curso.CURSOID as id,
+                curso.CURSO_Nombre as nombre,
+                CAST(curso.CURSO_Descripcion AS VARCHAR(MAX)) as descripcion,
+                curso.CURSO_Tipo + ' ' + curso.CURSO_Modalidad as tipo,
+                STRING_AGG(area.AREA_Siglas, ',') AS areas,
+                curso.CURSO_Total_Horas as duracion,
+                curso.CURSO_Fecha_Inicio as inicio,
+                curso.CURSO_Fecha_Final as final,
+                usuario.USER_Nombre + ' ' + usuario.USER_Apellido AS instructor
+            FROM
+                tblCurso AS curso
+            LEFT JOIN
+                tblCursoInstructor AS cursoInstructor ON curso.CURSOID = cursoInstructor.CURSOID
+            LEFT JOIN
+                tblInstructor AS instructor ON instructor.INSTRUCTORID = cursoInstructor.INSTRUCTORID
+            LEFT JOIN
+                tblUsuario AS usuario ON usuario.USERID = instructor.USERID
+            LEFT JOIN
+                tblCursoArea AS cursoArea ON cursoArea.CURSOID = curso.CURSOID
+            LEFT JOIN
+                tblArea AS area ON area.AREAID = cursoArea.AREAID
+            WHERE
+                curso.CURSOID = ? AND
+                curso.CURSO_Activo = 1
+            GROUP BY
+                curso.CURSOID,
+                curso.CURSO_Nombre,
+                CAST(curso.CURSO_Descripcion AS VARCHAR(MAX)),
+                curso.CURSO_Tipo,
+                curso.CURSO_Modalidad,
+                curso.CURSO_Total_Horas,
+                curso.CURSO_Fecha_Inicio,
+                curso.CURSO_Fecha_Final,
+                usuario.USER_Nombre,
+                usuario.USER_Apellido;",
+            [$cursoId]
+        )->getOrFail();
+    }
 }
