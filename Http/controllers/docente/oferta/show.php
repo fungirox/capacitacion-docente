0bf54673438c835a2a4e5b487372581b7d105642
Diego Rascón <jose.d.rascon@gmail.com>
2024-11-27 22:37:59 -0700

    Se agregó htmlspecialchars a las propiedades de la base de datos

--- Http/controllers/docente/oferta/show.php
+++ Http/controllers/docente/oferta/show.php
@@ -7,13 +7,14 @@ use Core\Session;
 $curso = App::resolve(CursoRepository::class)->getUnsubscribedById($_GET["id"], Session::getUser("id"));
 
 return view("docente/oferta/show.view.php", [
-    "id" => $curso["id"],
-    "nombre" => $curso["nombre"],
-    "descripcion" => $curso["descripcion"],
-    "tipo" => $curso["tipo"],
-    "areas" => explode(",", $curso["areas"]),
-    "duracion" => $curso["duracion"],
-    "inicio" => formatDate($curso["inicio"]),
-    "final" => formatDate($curso["final"]),
-    "instructor" => $curso["instructor_nombre"]
+    "id" => htmlspecialchars($curso["id"]),
+    "nombre" => htmlspecialchars($curso["nombre"]),
+    "descripcion" => htmlspecialchars($curso["descripcion"]),
+    "tipo" => htmlspecialchars($curso["tipo"]),
+    "areas" => explode(",", htmlspecialchars($curso["areas"])),
+    "duracion" => htmlspecialchars($curso["duracion"]),
+    "inicio" => formatDate(htmlspecialchars($curso["inicio"])),
+    "final" => formatDate(htmlspecialchars($curso["final"])),
+    "aula" => htmlspecialchars($curso["aula"]),
+    "instructor" => htmlspecialchars($curso["instructor_nombre"])
 ]);
