36ccde56f07294dca6e92a42500c7f8b025f8d9e
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-03 15:25:50 -0700

    Se modificó el getAll para la paginación y sorting

--- Core/Repositories/AreaRepository.php
+++ Core/Repositories/AreaRepository.php
@@ -4,17 +4,66 @@ namespace Core\Repositories;
 
 class AreaRepository extends RepositoryTemplate {
 
-    public function getAll($archivado = 0) {
-        return $this->query(
+    public function getAll($archivado = 0, $page = 1, $limit = 15, $search = "", $sortBy = "AREA_Nombre", $sortOrder = "ASC") {
+        $allowedSortColumns = ['AREA_Nombre', 'AREA_Siglas'];
+
+        $sortBy = in_array($sortBy, $allowedSortColumns) ? $sortBy : 'AREA_Nombre';
+        $sortOrder = in_array($sortOrder, $this->validOrders) ? $sortOrder : 'ASC';
+
+        $searchCondition = $search ? "AND (AREA_Nombre LIKE ? OR AREA_Siglas LIKE ?)" : "";
+
+        $params = [$archivado];
+
+        if ($search) {
+            $searchParam = "%$search%";
+            $params[] = $searchParam;
+            $params[] = $searchParam;
+        }
+
+        $countParams = [$archivado];
+        if ($search) {
+            $searchParam = "%{$search}%";
+            $countParams[] = $searchParam;
+            $countParams[] = $searchParam;
+        }
+
+        $totalCount = $this->query(
+            "SELECT COUNT(*) as total 
+            FROM tblArea 
+            WHERE AREA_Archivado = ?
+            $searchCondition",
+            $countParams
+        )->get()['total'];
+
+        $totalPages = max(1, ceil($totalCount / $limit));
+
+        $page = max(1, min($page, $totalPages));
+
+        $params[] = $this->getOffset($page, $limit);
+        $params[] = $limit;
+
+        $results = $this->query(
             "SELECT
                 AREAID as id,
                 AREA_Nombre as nombre,
                 AREA_Siglas as siglas
             FROM tblArea
             WHERE AREA_Archivado = ?
-            ORDER BY Area_Nombre",
-            [$archivado]
+            $searchCondition
+            ORDER BY $sortBy $sortOrder
+            OFFSET CAST(? AS INT) ROWS
+            FETCH NEXT CAST(? AS INT) ROWS ONLY",
+            $params
         )->getAll();
+
+        return [
+            "data" => $results,
+            "pagination" => [
+                "totalItems" => $totalCount,
+                "totalPages" => $totalPages,
+                "currentPage" => $page,
+            ]
+        ];
     }
 
     public function getById($id) {
