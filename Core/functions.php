e505f276634b85887b39ac7d2a30689129931eb2
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-03 23:00:50 -0700

    Se agregaron funciones para formatear días

--- Core/functions.php
+++ Core/functions.php
@@ -64,10 +64,89 @@ function formatDate($sqlDate) {
     return sprintf("%s %s, %s", $months[$monthIndex], $day, $year);
 }
 
+function fullFormattedDays($dias) {
+    $dayOrder = [
+        'lunes' => 1,
+        'martes' => 2,
+        'miercoles' => 3,
+        'jueves' => 4,
+        'viernes' => 5,
+        'sabado' => 6,
+        'domingo' => 7
+    ];
+
+    $diasArray = explode(",", $dias);
+
+    usort($diasArray, function ($a, $b) use ($dayOrder) {
+        $orderA = $dayOrder[strtolower($a)] ?? PHP_INT_MAX;
+        $orderB = $dayOrder[strtolower($b)] ?? PHP_INT_MAX;
+        return $orderA - $orderB;
+    });
+
+    $diasArray = array_map(function ($dia) {
+        return str_replace('miercoles', 'miércoles', $dia);
+    }, $diasArray);
+
+    $result = count($diasArray) > 1
+        ? implode(', ', array_slice($diasArray, 0, -1)) . ' y ' . end($diasArray)
+        : $diasArray[0];
+
+    return ucfirst($result);
+}
+
+function shortFormattedDays($dias) {
+    $dayOrder = [
+        'lunes' => 1,
+        'martes' => 2,
+        'miercoles' => 3,
+        'jueves' => 4,
+        'viernes' => 5,
+        'sabado' => 6,
+        'domingo' => 7
+    ];
+
+    $diasMap = [
+        'lunes' => 'LU',
+        'martes' => 'MA',
+        'miercoles' => 'MI',
+        'jueves' => 'JU',
+        'viernes' => 'VI',
+        'sabado' => 'SA',
+        'domingo' => 'DO'
+    ];
+
+    $diasArray = explode(",", $dias);
+
+    $diasAbreviados = array_map(function ($dia) use ($diasMap) {
+        return $diasMap[$dia] ?? $dia;
+    }, $diasArray);
+
+    usort($diasAbreviados, function ($a, $b) use ($dayOrder, $diasMap) {
+        $originalA = array_search($a, $diasMap);
+        $originalB = array_search($b, $diasMap);
+
+        $orderA = $dayOrder[$originalA] ?? PHP_INT_MAX;
+        $orderB = $dayOrder[$originalB] ?? PHP_INT_MAX;
+
+        return $orderA - $orderB;
+    });
+
+    return implode(' ', $diasAbreviados);
+}
+
+
 function formattedDateRange($startDate, $endDate) {
     if (substr($startDate, -4) === substr($startDate, -4)) {
         return substr($startDate, 0, -6) . " - " . $endDate;
     }
 
     return $startDate . " - " - $endDate;
+}
+
+function getAmOrPm($hour) {
+    return $hour < 12 ? "AM" : "PM";
+}
+
+function formattedHourRange($startHour, $endHour) {
+    return substr($startHour, 0, 5) . " " . getAmOrPm($startHour) . " - " . substr($endHour, 0, 5) . " " . getAmOrPm($endHour);
 }
