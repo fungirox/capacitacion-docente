9d56a21038cd62844b4049b34a1c94b4887b41f4
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-05 12:32:55 -0700

    Se envían los parámetros para paginación y sorting

--- Http/controllers/admin/cursos/index.php
+++ Http/controllers/admin/cursos/index.php
@@ -3,14 +3,38 @@
 use Core\App;
 use Core\Repositories\CursoRepository;
 
-$allCursos = App::resolve(CursoRepository::class)->getAll();
+$archivados = isset($_GET["archivados"]);
+$page = $_GET["page"] ?? 1;
+$search = $_GET["search"] ?? "";
 
-foreach ($allCursos as $key => $curso) {
+$sortInput = $_GET["sortBy"] ?? 'CURSOID-DESC';
+$sortParts = explode('-', $sortInput);
+$sortBy = $sortParts[0];
+$sortOrder = $sortParts[1];
+
+$cursosData = App::resolve(CursoRepository::class)->getAll(
+    $archivados ? 1 : 0,
+    $page,
+    3,
+    $search,
+    $sortBy,
+    $sortOrder
+);
+
+foreach ($cursosData["data"] as $key => $curso) {
     $areas = explode(",", $curso["areas"]);
-    $allCursos[$key]["areas"] = $areas;
+    $cursosData["data"][$key]["areas"] = $areas;
 }
+
+$paramsActive = isset($_GET["search"]) && !empty($_GET["search"]);
 
 return view("admin/cursos/index.view.php", [
     "title" => "Cursos",
-    "allCourses" => $allCursos
+    "allCourses" => $cursosData["data"],
+    "paramsActive" => $paramsActive,
+    "pagination" => $cursosData["pagination"],
+    "archivados" => $archivados,
+    "search" => $search,
+    "sortBy" => $sortBy,
+    "sortOrder" => $sortOrder
 ]);
