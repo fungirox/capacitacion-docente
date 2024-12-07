9585082d54d7b8e3a6e330bd52b4f4d233b480bd
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-07 00:08:42 -0700

    Se envían los parámetros correctos

--- Http/controllers/docente/oferta/index.php
+++ Http/controllers/docente/oferta/index.php
@@ -4,14 +4,40 @@ use Core\App;
 use Core\Repositories\CursoRepository;
 use Core\Session;
 
-$allCursos = App::resolve(CursoRepository::class)->getAllUnsubscribed(Session::getUser("id"));
+$page = $_GET["page"] ?? 1;
+$search = $_GET["search"] ?? "";
 
-foreach ($allCursos as $key => $curso) {
+$sortInput = $_GET["sortBy"] ?? 'CURSOID-DESC';
+$sortParts = explode('-', $sortInput);
+$sortBy = $sortParts[0];
+$sortOrder = $sortParts[1];
+
+$filterBy = $_GET["filterBy"] ?? null;
+
+$cursosData = App::resolve(CursoRepository::class)->getAllUnsubscribed(
+    $page,
+    15,
+    $search,
+    $sortBy,
+    $sortOrder,
+    $filterBy,
+    Session::getUser("id")
+);
+
+foreach ($cursosData["data"] as $key => $curso) {
     $areas = explode(",", $curso["areas"]);
-    $allCursos[$key]["areas"] = $areas;
+    $cursosData["data"][$key]["areas"] = $areas;
 }
 
-return view("/docente/oferta/index.view.php", [
+$paramsActive = (isset($_GET["search"]) && !empty($_GET["search"])) || (isset($_GET["filterBy"]) && !empty($_GET["filterBy"]));
+
+return view("docente/oferta/index.view.php", [
     "title" => "Oferta de Cursos",
-    "allCursos" => $allCursos,
+    "allCursos" => $cursosData["data"],
+    "paramsActive" => $paramsActive,
+    "pagination" => $cursosData["pagination"],
+    "search" => $search,
+    "sortBy" => $sortBy,
+    "sortOrder" => $sortOrder,
+    "filterBy" => $filterBy
 ]);
