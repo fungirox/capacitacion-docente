67699868ae5fab47724093a7697d4e0b929cf630
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-08 19:02:24 -0700

    Componente para mostrar un título con flecha

---
+++ views/components/nestedTitle.php
@@ -0,0 +1,6 @@
+<div class="d-flex align-items-center gap-3 mb-2">
+    <a href="<?= $url ?>">
+        <i class="col bi bi-arrow-left-circle fs-3"></i>
+    </a>
+    <h1 class="m-0 p-0"><?= $title ?></h1>
+</div>
