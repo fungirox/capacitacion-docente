a22d0b39b170f8d8dfcb7af979fc7dcac35431f5
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-09 19:42:48 -0700

    Se agregó la función para obtener un curso instruyendo

--- Core/Repositories/CursoRepository.php
+++ Core/Repositories/CursoRepository.php
@@ -591,6 +591,75 @@ class CursoRepository extends RepositoryTemplate {
         )->getOrFail();
     }
 
+    public function getAllTeachingById($cursoId, $userId) {
+        return $this->query(
+            "SELECT
+                curso.CURSOID as id,
+                curso.CURSO_Nombre as nombre,
+                CAST(curso.CURSO_Descripcion AS VARCHAR(MAX)) as descripcion,
+                curso.CURSO_Tipo + ' ' + curso.CURSO_Modalidad as tipo,
+                curso.CURSO_Modalidad as modalidad,
+                (
+                    SELECT STRING_AGG(AREA_Siglas, ',') WITHIN GROUP (ORDER BY AREA_Siglas)
+                    FROM (
+                        SELECT DISTINCT area.AREA_Siglas
+                        FROM tblCursoArea AS cursoArea
+                        JOIN tblArea AS area ON area.AREAID = cursoArea.AREAID
+                        WHERE cursoArea.CURSOID = curso.CURSOID
+                    ) AS distinct_areas
+                ) AS areas,
+                (
+                    SELECT STRING_AGG(HORARIOCURSO_Dia_Semana, ',') WITHIN GROUP (ORDER BY HORARIOCURSO_Dia_Semana)
+                    FROM (
+                        SELECT DISTINCT horario.HORARIOCURSO_Dia_Semana
+                        FROM tblHorarioCurso AS horario
+                        WHERE horario.CURSOID = curso.CURSOID
+                    ) AS distinct_dias
+                ) AS dias,
+                (
+                    SELECT TOP 1 horario.HORARIOCURSO_Hora_Inicio
+                    FROM tblHorarioCurso AS horario
+                    WHERE horario.CURSOID = curso.CURSOID
+                ) AS hora_inicial,
+                (
+                    SELECT TOP 1 horario.HORARIOCURSO_Hora_Final
+                    FROM tblHorarioCurso AS horario
+                    WHERE horario.CURSOID = curso.CURSOID
+                ) AS hora_final,
+                curso.CURSO_Total_Horas as duracion,
+                curso.CURSO_Fecha_Inicio as inicio,
+                curso.CURSO_Fecha_Final as final,
+                curso.CURSO_Aula as aula,
+                usuario.USER_Nombre + ' ' + usuario.USER_Apellido AS instructor_nombre
+            FROM
+                tblCurso AS curso
+            LEFT JOIN
+                tblCursoInstructor AS curso_instructor ON curso.CURSOID = curso_instructor.CURSOID
+            LEFT JOIN
+                tblInstructor AS instructor ON instructor.INSTRUCTORID = curso_instructor.INSTRUCTORID
+            LEFT JOIN
+                tblUsuario AS usuario ON usuario.USERID = instructor.USERID
+            WHERE
+                curso.CURSOID = ? AND
+                curso.CURSO_Archivado = 0 AND
+                curso.CURSO_Activo = 1 AND
+                instructor.USERID = ?
+            GROUP BY
+                curso.CURSOID,
+                curso.CURSO_Nombre,
+                CAST(curso.CURSO_Descripcion AS VARCHAR(MAX)),
+                curso.CURSO_Tipo,
+                curso.CURSO_Modalidad,
+                curso.CURSO_Total_Horas,
+                curso.CURSO_Fecha_Inicio,
+                curso.CURSO_Fecha_Final,
+                curso.CURSO_Aula,
+                usuario.USER_Nombre,
+                usuario.USER_Apellido",
+            [$cursoId, $userId]
+        )->getOrFail();
+    }
+
     public function archive($id, $state) {
         return $this->query(
             "UPDATE tblCurso SET CURSO_Archivado = ? WHERE CURSOID = ?",
