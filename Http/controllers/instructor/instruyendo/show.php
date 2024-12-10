764dc0368f21aa0307e342de80e35fb9f9dd4dc4
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-09 19:43:57 -0700

    Se agregó el controlador y la vista para mostrar los detalles de un curso instruyendo

---
+++ Http/controllers/instructor/instruyendo/show.php
@@ -0,0 +1,22 @@
+<?php
+
+use Core\App;
+use Core\Repositories\CursoRepository;
+use Core\Session;
+
+$curso = App::resolve(CursoRepository::class)->getAllTeachingById($_GET["id"], Session::getUser("id"));
+
+return view("instructor/instruyendo/show.view.php", [
+    "id" => htmlspecialchars($curso["id"]),
+    "nombre" => htmlspecialchars($curso["nombre"]),
+    "descripcion" => htmlspecialchars($curso["descripcion"]),
+    "tipo" => htmlspecialchars(ucfirst($curso["tipo"])),
+    "isVirtual" => htmlspecialchars($curso["modalidad"] == "virtual"),
+    "areas" => explode(",", htmlspecialchars($curso["areas"])),
+    "duracion" => htmlspecialchars($curso["duracion"]),
+    "fechas" => htmlspecialchars(formattedDateRange(formatDate($curso["inicio"]), formatDate($curso["final"]))),
+    "dias" => htmlspecialchars(fullFormattedDays($curso["dias"])),
+    "horas" => htmlspecialchars(formattedHourRange($curso["hora_inicial"], $curso["hora_final"])),
+    "aula" => htmlspecialchars($curso["aula"]),
+    "instructor" => htmlspecialchars($curso["instructor_nombre"])
+]);
