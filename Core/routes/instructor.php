e122210cb380982aea2fd7caf1de94d2e7fc0304
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-09 19:43:04 -0700

    Se agregó la ruta para ver un curso en específico

--- Core/routes/instructor.php
+++ Core/routes/instructor.php
@@ -7,3 +7,4 @@ const INSTRUCTOR = Roles::INSTRUCTOR;
 # Instruyendo
 
 $router->get("/instruyendo", "instructor/instruyendo/index.php")->only(INSTRUCTOR);
+$router->get("/instruyendo/curso", "instructor/instruyendo/show.php")->only(INSTRUCTOR);
