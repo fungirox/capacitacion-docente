7eb52fef4ff7281435439f9757fc1c10b5d7e876
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-11 10:04:39 -0700

    Se envían los errores a la vista

--- Http/controllers/admin/cursos/create.php
+++ Http/controllers/admin/cursos/create.php
@@ -3,12 +3,14 @@
 use Core\App;
 use Core\Repositories\AreaRepository;
 use Core\Repositories\InstructorRepository;
+use Core\Session;
 
 $instructores = App::resolve(InstructorRepository::class)->getAll();
 $areas = App::resolve(AreaRepository::class)->getAll();
 
 return view("admin/cursos/create.view.php", [
     "title" => "Registrar Servicio",
+    "errors" => Session::get("errors"),
     "instructores" => $instructores,
     "areas" => $areas,
 ]);
