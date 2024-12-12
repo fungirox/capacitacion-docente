ee2f137b5d0b5b32aa05a3d75738b9862f286b77
Diego Rascón <jose.d.rascon@gmail.com>
2024-12-11 21:42:28 -0700

    Se comenzó con la validación del formulario para actualizar un servicio

--- Http/Forms/ServicioForm.php
+++ Http/Forms/ServicioForm.php
@@ -7,20 +7,20 @@ use Core\Validator;
 class ServicioForm extends FormTemplate {
 
     protected function validateForm() {
-        if (!Validator::servicio($this->attributes["tipo"])) {
-            $this->errors['tipo'] = "Favor de seleccionar un tipo de servicio válido.";
+        if (!Validator::tipoDeServicio($this->attributes["tipo"])) {
+            $this->errors["tipo"] = "Favor de seleccionar un tipo de servicio válido.";
         }
 
         if (!Validator::string($this->attributes["nombre"], 1, 100)) {
-            $this->errors['nombre'] = "Favor de introducir el nombre del servicio.";
+            $this->errors["nombre"] = "Favor de introducir el nombre del servicio.";
         }
 
-        if (!Validator::string($this->attributes["descripcion"], 1, 100)) {
-            $this->errors['descripcion'] = "Favor de introducir una descripción para el servicio.";
+        if (!Validator::string($this->attributes["descripcion"], 1)) {
+            $this->errors["descripcion"] = "Favor de introducir una descripción para el servicio.";
         }
 
-        // if (!Validator::string($this->attributes["siglas"], 1, 5)) {
-        //     $this->errors['siglas'] = "Favor de introducir las siglas del área.";
-        // }
+        if (!Validator::isValidInstructor($this->attributes["instructor"])) {
+            $this->errors["instructor"] = "Favor de seleccionar un instructor de la lista.";
+        }
     }
 }
