07320b9c62ee4bc8402b43fffd5b17709e7dda48
Diego Rascón <jose.d.rascon@gmail.com>
2025-01-02 17:55:20 -0700

    Ahora se usa el componente de vista de curso

--- Http/controllers/admin/cursos/show.php
+++ Http/controllers/admin/cursos/show.php
@@ -5,17 +5,21 @@ use Core\Repositories\CursoRepository;
 
 $curso = App::resolve(CursoRepository::class)->getById($_GET["id"]);
 
-return view("admin/cursos/show.view.php", [
+return view("components/curso.php", [
     "id" => htmlspecialchars($curso["id"]),
     "nombre" => htmlspecialchars($curso["nombre"]),
     "descripcion" => htmlspecialchars($curso["descripcion"]),
     "tipo" => htmlspecialchars(ucfirst($curso["tipo"])),
     "isVirtual" => htmlspecialchars($curso["modalidad"] == "virtual"),
     "areas" => explode(",", htmlspecialchars($curso["areas"])),
+    "origen" => htmlspecialchars($curso["origen"]),
+    "perfil" => htmlspecialchars($curso["perfil"]),
     "duracion" => htmlspecialchars($curso["duracion"]),
     "fechas" => htmlspecialchars(formattedDateRange(formatDate($curso["inicio"]), formatDate($curso["final"]))),
     "dias" => htmlspecialchars(fullFormattedDays($curso["dias"])),
     "horas" => htmlspecialchars(formattedHourRange($curso["hora_inicial"], $curso["hora_final"])),
     "aula" => htmlspecialchars($curso["aula"]),
-    "instructor" => htmlspecialchars($curso["instructor_nombre"])
+    "limite" => htmlspecialchars($curso["limite"]),
+    "instructor" => htmlspecialchars($curso["instructor_nombre"]),
+    "mode" => "admin"
 ]);
--- Http/controllers/instructor/instruyendo/show.php
+++ Http/controllers/instructor/instruyendo/show.php
@@ -6,17 +6,21 @@ use Core\Session;
 
 $curso = App::resolve(CursoRepository::class)->getAllTeachingById($_GET["id"], Session::getUser("id"));
 
-return view("instructor/instruyendo/show.view.php", [
+return view("components/curso.php", [
     "id" => htmlspecialchars($curso["id"]),
     "nombre" => htmlspecialchars($curso["nombre"]),
     "descripcion" => htmlspecialchars($curso["descripcion"]),
     "tipo" => htmlspecialchars(ucfirst($curso["tipo"])),
     "isVirtual" => htmlspecialchars($curso["modalidad"] == "virtual"),
     "areas" => explode(",", htmlspecialchars($curso["areas"])),
+    "origen" => htmlspecialchars($curso["origen"]),
+    "perfil" => htmlspecialchars($curso["perfil"]),
     "duracion" => htmlspecialchars($curso["duracion"]),
     "fechas" => htmlspecialchars(formattedDateRange(formatDate($curso["inicio"]), formatDate($curso["final"]))),
     "dias" => htmlspecialchars(fullFormattedDays($curso["dias"])),
     "horas" => htmlspecialchars(formattedHourRange($curso["hora_inicial"], $curso["hora_final"])),
     "aula" => htmlspecialchars($curso["aula"]),
-    "instructor" => htmlspecialchars($curso["instructor_nombre"])
+    "limite" => htmlspecialchars($curso["limite"]),
+    "instructor" => htmlspecialchars($curso["instructor_nombre"]),
+    "mode" => "inscrito"
 ]);
