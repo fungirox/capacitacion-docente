0f15208f2b58f124a03012e2cc15e59adfaccbb5
Diego Rascón <jose.d.rascon@gmail.com>
2025-01-29 23:09:39 -0700

    Formulario de administrador

---
+++ Http/Forms/AdministradorForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Http\Forms;
+
+use Core\Validator;
+
+class AdministradorForm extends FormTemplate {
+
+    protected function validateForm() {
+        if (!Validator::string($this->attributes["nombre"])) {
+            $this->errors['nombre'] = "Favor de introducir un nombre válido.";
+        }
+
+        if (!Validator::string($this->attributes["apellido"])) {
+            $this->errors['apellido'] = "Favor de introducir un apellido válido.";
+        }
+
+        if (!Validator::string($this->attributes["username"])) {
+            $this->errors['username'] = "Favor de introducir un nombre de usuario válido.";
+        }
+
+        if (!Validator::email($this->attributes["email"])) {
+            $this->errors['email'] = "Favor de introducir un correo electrónico válido.";
+        }
+
+        if (!Validator::bit($this->attributes["genero"])) {
+            $this->errors['genero'] = "Favor de seleccionar un género válido.";
+        }
+
+        if (!Validator::string($this->attributes["password"], 5)) {
+            $this->errors['password'] = "La contraseña debe tener al menos 5 caracteres.";
+        }
+
+        if ($this->attributes["password"] !== $this->attributes["confirmPassword"]) {
+            $this->errors['confirmPassword'] = "Las contraseñas no coinciden.";
+        }
+    }
+}
