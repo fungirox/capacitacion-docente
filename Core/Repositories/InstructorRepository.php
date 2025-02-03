3bac010873ddf830f5b2bc88fa84bff73fccf742
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-02 21:32:15 -0700

    Se agregó el método para obtener con parámetros

--- Core/Repositories/InstructorRepository.php
+++ Core/Repositories/InstructorRepository.php
@@ -4,28 +4,94 @@ namespace Core\Repositories;
 
 class InstructorRepository extends RepositoryTemplate {
 
-    public function getAll() {
-        return $this->query(
-            "SELECT 
-                INSTRUCTORID as id,
-                USER_Nombre + ' ' + USER_Apellido AS nombre
-            FROM tblInstructor AS instructor
-            LEFT JOIN tblUsuario AS usuario ON instructor.USERID = usuario.USERID
-            WHERE USER_Activo = 1
-            ORDER BY USER_Nombre + ' ' + USER_Apellido ASC"
-        )->getAll();
-    }
+    public function getAllWithParams($archivado = 0, $page = 1, $limit = 15, $search = "", $sortBy = "usuario.USERID-DESC", $sortOrder = "ASC") {
+        $allowedSortColumns = ['usuario.USERID', 'USER_NombreUsuario', 'USER_Nombre', 'USER_Apellido', 'USER_Email'];
 
-    public function getAllIds($instructorId) {
-        return $this->query(
-            "SELECT 
-                INSTRUCTORID as id
-            FROM tblInstructor AS instructor
-            LEFT JOIN tblUsuario AS usuario ON instructor.USERID = usuario.USERID
-            WHERE USER_Activo = 1 AND
-            INSTRUCTORID = ?
-            ORDER BY USER_Nombre + ' ' + USER_Apellido ASC",
-            [$instructorId]
+        $sortBy = in_array($sortBy, $allowedSortColumns) ? $sortBy : 'usuario.USERID';
+        $sortOrder = in_array($sortOrder, $this->validOrders) ? $sortOrder : 'DESC';
+
+        $searchCondition = $search ? "AND (USER_NombreUsuario LIKE ? OR USER_Nombre + ' ' + USER_Apellido LIKE ? OR USER_Email LIKE ?)" : "";
+
+        $params = [$archivado];
+
+        if ($search) {
+            $searchParam = "%$search%";
+            $params[] = $searchParam;
+            $params[] = $searchParam;
+            $params[] = $searchParam;
+        }
+
+        $countParams = [$archivado];
+        if ($search) {
+            $searchParam = "%{$search}%";
+            $countParams[] = $searchParam;
+            $countParams[] = $searchParam;
+            $countParams[] = $searchParam;
+        }
+
+        $totalCount = $this->query(
+            "SELECT COUNT(*) as total 
+            FROM tblUsuario AS usuario
+            INNER JOIN tblInstructor AS instructor ON instructor.USERID = usuario.USERID
+            WHERE USER_Activo != ?
+            $searchCondition",
+            $countParams
+        )->get()['total'];
+
+        $totalPages = max(1, ceil($totalCount / $limit));
+
+        $page = max(1, min($page, $totalPages));
+
+        $params[] = $this->getOffset($page, $limit);
+        $params[] = $limit;
+
+        $results = $this->query(
+            "SELECT
+                usuario.USERID AS id,
+                USER_NombreUsuario AS username,
+                USER_Nombre + ' ' + USER_Apellido AS nombre,
+                USER_Email AS email
+            FROM tblUsuario AS usuario
+            INNER JOIN tblInstructor AS instructor ON instructor.USERID = usuario.USERID
+            WHERE USER_Activo != ?
+            $searchCondition
+            ORDER BY $sortBy $sortOrder
+            OFFSET CAST(? AS INT) ROWS
+            FETCH NEXT CAST(? AS INT) ROWS ONLY",
+            $params
         )->getAll();
+
+        return [
+            "data" => $results,
+            "pagination" => [
+                "totalItems" => $totalCount,
+                "totalPages" => $totalPages,
+                "currentPage" => $page,
+            ]
+        ];
     }
+    // public function getAll() {
+    //     return $this->query(
+    //         "SELECT 
+    //             INSTRUCTORID as id,
+    //             USER_Nombre + ' ' + USER_Apellido AS nombre
+    //         FROM tblInstructor AS instructor
+    //         LEFT JOIN tblUsuario AS usuario ON instructor.USERID = usuario.USERID
+    //         WHERE USER_Activo = 1
+    //         ORDER BY USER_Nombre + ' ' + USER_Apellido ASC"
+    //     )->getAll();
+    // }
+
+    // public function getAllIds($instructorId) {
+    //     return $this->query(
+    //         "SELECT 
+    //             INSTRUCTORID as id
+    //         FROM tblInstructor AS instructor
+    //         LEFT JOIN tblUsuario AS usuario ON instructor.USERID = usuario.USERID
+    //         WHERE USER_Activo = 1 AND
+    //         INSTRUCTORID = ?
+    //         ORDER BY USER_Nombre + ' ' + USER_Apellido ASC",
+    //         [$instructorId]
+    //     )->getAll();
+    // }
 }
