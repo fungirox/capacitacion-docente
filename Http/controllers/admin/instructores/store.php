efd602d15d54f3d0a6b38c1316fa930749956f2c
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-03 13:56:42 -0700

    Controlador para almacenar instructores

---
+++ Http/controllers/admin/instructores/store.php
@@ -0,0 +1,20 @@
+<?php
+
+use Core\App;
+use Core\Repositories\InstructorRepository;
+use Http\Forms\InstructorForm;
+
+InstructorForm::validate($attributes = [
+    "nombre" => trim($_POST["nombre"]),
+    "apellido" => trim($_POST["apellido"]),
+    "username" => trim($_POST["username"]),
+    "email" => trim($_POST["email"]),
+    "genero" => $_POST["genero"],
+    "estudios" => $_POST["estudios"],
+    "password" => trim($_POST["password"]),
+    "confirmPassword" => trim($_POST["confirm-password"])
+]);
+
+App::resolve(InstructorRepository::class)->create($attributes);
+
+redirect("/admin/instructores");
