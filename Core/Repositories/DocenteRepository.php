3d002b1abe0035ce211c20f5ec1f9e96e5eb3794
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-04 23:23:12 -0700

    Se agregó la función para obtener todos los docentes con parámetros

--- Core/Repositories/DocenteRepository.php
+++ Core/Repositories/DocenteRepository.php
@@ -5,11 +5,86 @@ namespace Core\Repositories;
 use Core\Roles\Roles;
 
 class DocenteRepository extends RepositoryTemplate {
-    private $admin = Roles::ADMIN;
-    private $docente = Roles::DOCENTE;
-    private $instructor = Roles::INSTRUCTOR;
-    private $docenteAndInstructor = Roles::DOCENTE_AND_INSTRUCTOR;
-    private $guest = Roles::GUEST;
+
+    public function getAllWithParams($archivado = 0, $page = 1, $limit = 15, $search = "", $sortBy = "usuario.USERID-DESC", $sortOrder = "ASC") {
+        $allowedSortColumns = ['usuario.USERID', 'USER_NombreUsuario', 'USER_Nombre', 'USER_Apellido', 'USER_Email'];
+
+        $sortBy = in_array($sortBy, $allowedSortColumns) ? $sortBy : 'usuario.USERID';
+        $sortOrder = in_array($sortOrder, $this->validOrders) ? $sortOrder : 'DESC';
+
+        $searchCondition = $search ? "AND (USER_NombreUsuario LIKE ? OR USER_Nombre + ' ' + USER_Apellido LIKE ? OR USER_Email LIKE ?)" : "";
+
+        $params = [$archivado];
+
+        if ($search) {
+            $searchParam = "%$search%";
+            $params[] = $searchParam;
+            $params[] = $searchParam;
+            $params[] = $searchParam;
+        }
+
+        $countParams = [$archivado];
+        if ($search) {
+            $searchParam = "%{$search}%";
+            $countParams[] = $searchParam;
+            $countParams[] = $searchParam;
+            $countParams[] = $searchParam;
+        }
+
+        $totalCount = $this->query(
+            "SELECT COUNT(*) as total 
+            FROM tblUsuario AS usuario
+            INNER JOIN tblDocente AS docente ON docente.USERID = usuario.USERID
+            WHERE USER_Activo != ?
+            $searchCondition",
+            $countParams
+        )->get()['total'];
+
+        $totalPages = max(1, ceil($totalCount / $limit));
+
+        $page = max(1, min($page, $totalPages));
+
+        $params[] = $this->getOffset($page, $limit);
+        $params[] = $limit;
+
+        $results = $this->query(
+            "SELECT
+                usuario.USERID AS id,
+                USER_NombreUsuario AS username,
+                USER_Nombre + ' ' + USER_Apellido AS nombre,
+                CASE
+                    WHEN EXISTS (
+                        SELECT 1
+                        FROM tblInstructor
+                        WHERE USERID = usuario.USERID
+                    ) THEN 'Instructor'
+                    ELSE ''
+                END AS es_instructor,
+                CASE
+                    WHEN DOCENTE_Base = 1
+                    THEN DOCENTE_Horas_Base
+                    ELSE 0
+                END AS horas_base,
+                USER_Email AS email
+            FROM tblUsuario AS usuario
+            INNER JOIN tblDocente AS docente ON docente.USERID = usuario.USERID
+            WHERE USER_Activo != ?
+            $searchCondition
+            ORDER BY $sortBy $sortOrder
+            OFFSET CAST(? AS INT) ROWS
+            FETCH NEXT CAST(? AS INT) ROWS ONLY",
+            $params
+        )->getAll();
+
+        return [
+            "data" => $results,
+            "pagination" => [
+                "totalItems" => $totalCount,
+                "totalPages" => $totalPages,
+                "currentPage" => $page,
+            ]
+        ];
+    }
 
     public function getAll() {
         return $this->query(
