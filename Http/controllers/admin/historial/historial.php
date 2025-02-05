c9a8b6af2970551373759e61a7a4abdec4a54227
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-04 23:27:16 -0700

    Se eliminaron las vistas que no se usaban de historial

--- Http/controllers/admin/historial/historial.php
+++
@@ -1,4 +0,0 @@
-<?php
-$title = "Historial";
-
-require view("admin/historial/historial.view.php");
--- views/admin/historial/historial.view.php
+++
@@ -1,5 +0,0 @@
-<?php require view("components/styledHeader.php"); ?>
-<main role="main" class="container py-5" style="margin-top: 56px">
-    <h1>Historial de Cursos</h1>
-</main>
-<?php require view("components/styledFooter.php"); ?>
