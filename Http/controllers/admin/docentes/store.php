d071c084bd04c9f0e9509cdeece90e5a6957dbd6
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-05 21:11:25 -0700

    Se simplificó el controlador para almacenar

--- Http/controllers/admin/docentes/store.php
+++ Http/controllers/admin/docentes/store.php
@@ -11,13 +11,12 @@ DocenteForm::validate($attributes = [
     "username" => trim($_POST["username"]),
     "email" => trim($_POST["email"]),
     "genero" => $_POST["genero"],
-    "estudios" => $_POST["estudios"],
     "password" => trim($_POST["password"]),
     "confirmPassword" => trim($_POST["confirm-password"]),
     "baseHoras" => $_POST["base-horas"] ?? 0,
     "horasBase" => $_POST["horas-base"] ?? 0,
     "docenteInstructor" => $_POST["docente-instructor"] ?? 0,
-    "estudios" => $_POST["estudios"]
+    "estudios" => trim($_POST["estudios"])
 ]);
 
 App::resolve(DocenteRepository::class)->create($attributes);
