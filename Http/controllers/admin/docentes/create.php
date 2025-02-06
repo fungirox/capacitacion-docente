85eddf7115af665ed6be7e487a3871efe4deda47
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-05 19:39:55 -0700

    Se envían los niveles de estudio

--- Http/controllers/admin/docentes/create.php
+++ Http/controllers/admin/docentes/create.php
@@ -5,4 +5,10 @@ use Core\Session;
 return view("admin/docentes/create.view.php", [
     "title" => "Registrar Docente",
     "errors" => Session::get("errors"),
+    "nivelEstudios" => [
+        "bachillerato" => "Bachillerato",
+        "licenciatura" => "Licenciatura",
+        "maestria" => "Maestría",
+        "doctorado" => "Doctorado",
+    ],
 ]);
