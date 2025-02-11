a9e3df221fdc76c4dc043594bafe26bc29a80319
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-10 19:29:44 -0700

    Se obtienen los alumnos

--- Http/controllers/instructor/instruyendo/asistencia/index.php
+++ Http/controllers/instructor/instruyendo/asistencia/index.php
@@ -2,11 +2,12 @@
 
 use Core\App;
 use Core\Repositories\CursoDocenteRepository;
+use Core\Session;
 
-$alumnos = App::resolve(CursoDocenteRepository::class)->getDocentesFromCurso($_GET["id"]);
+$alumnos = App::resolve(CursoDocenteRepository::class)->getDocentesFromCurso($_GET["id"], Session::getUser("id"));
 
 return view("/instructor/instruyendo/asistencia/index.view.php", [
     "title" => "Asistencia",
+    "id" => $_GET["id"],
     "alumnos" => $alumnos
 ]);
-
