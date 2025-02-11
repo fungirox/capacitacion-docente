9a5bab0eb34b79475c7e999cbb17b855671d0a4a
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-10 19:29:19 -0700

    Se revisa si el usuario actual es el instructor del curso

--- Core/Repositories/CursoDocenteRepository.php
+++ Core/Repositories/CursoDocenteRepository.php
@@ -4,17 +4,20 @@ namespace Core\Repositories;
 
 class CursoDocenteRepository extends RepositoryTemplate {
 
-    public function getDocentesFromCurso($cursoId) {
+    public function getDocentesFromCurso($cursoId, $userId) {
         return $this->query(
             "SELECT
                 docente.DOCENTEID AS id,
                 USER_Nombre + ' ' + USER_Apellido AS nombre
             FROM tblCursoDocente AS curso_docente
-            LEFT JOIN tblDocente AS docente ON curso_docente.DOCENTEID = docente.DOCENTEID
-            LEFT JOIN tblUsuario AS usuario ON docente.USERID = usuario.USERID
-            WHERE curso_docente.CURSOID = ?
+                INNER JOIN tblDocente AS docente ON curso_docente.DOCENTEID = docente.DOCENTEID
+                INNER JOIN tblUsuario AS usuario ON docente.USERID = usuario.USERID
+                INNER JOIN tblCursoInstructor AS curso_instructor ON curso_instructor.CURSOID = curso_docente.CURSOID
+                INNER JOIN tblInstructor AS instructor ON instructor.INSTRUCTORID = curso_instructor.INSTRUCTORID
+            WHERE curso_docente.CURSOID = ? AND
+                instructor.USERID = ?
             ORDER BY nombre ASC",
-            [$cursoId]
+            [$cursoId, $userId]
         )->getAll();
     }
 
