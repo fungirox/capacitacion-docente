29375872afd971fbd8277ddb10ccc4cf941ec173
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-11 10:56:33 -0700

    Se agregaron los títulos

--- Http/controllers/admin/personal/create.php
+++ Http/controllers/admin/personal/create.php
@@ -4,5 +4,11 @@ use Core\Session;
 
 return view("/admin/personal/create.view.php", [
     "title" => "Nuevo Personal",
-    "errors" => Session::get("errors")
+    "errors" => Session::get("errors"),
+    "titulos" => [
+        "Lic" => "Licenciado",
+        "Ing" => "Ingeniero",
+        "Mtro" => "Maestro",
+        "Dr" => "Doctor"
+    ]
 ]);
