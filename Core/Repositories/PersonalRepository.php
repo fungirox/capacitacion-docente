8067d255b98cb15860876acf38e1fd2705f74bb0
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-11 10:33:49 -0700

    Se agregó la función para obtener con parámetros

--- Core/Repositories/PersonalRepository.php
+++ Core/Repositories/PersonalRepository.php
@@ -16,6 +16,66 @@ class PersonalRepository extends RepositoryTemplate {
         )->getAll();
     }
 
+    public function getAllWithParams($archivado = 0, $page = 1, $limit = 15, $search = "", $sortBy = "PERSONAL_Nombre", $sortOrder = "ASC") {
+        $allowedSortColumns = ['PERSONAL_Nombre'];
+
+        $sortBy = in_array($sortBy, $allowedSortColumns) ? $sortBy : 'PERSONAL_Nombre';
+        $sortOrder = in_array($sortOrder, $this->validOrders) ? $sortOrder : 'ASC';
+
+        $searchCondition = $search ? "AND (PERSONAL_Nombre LIKE ?)" : "";
+
+        $params = [$archivado];
+
+        if ($search) {
+            $searchParam = "%$search%";
+            $params[] = $searchParam;
+        }
+
+        $countParams = [$archivado];
+        if ($search) {
+            $searchParam = "%{$search}%";
+            $countParams[] = $searchParam;
+        }
+
+        $totalCount = $this->query(
+            "SELECT COUNT(*) as total 
+            FROM tblPersonal 
+            WHERE PERSONAL_Archivado = ?
+            $searchCondition",
+            $countParams
+        )->get()['total'];
+
+        $totalPages = max(1, ceil($totalCount / $limit));
+
+        $page = max(1, min($page, $totalPages));
+
+        $params[] = $this->getOffset($page, $limit);
+        $params[] = $limit;
+
+        $results = $this->query(
+            "SELECT
+                PERSONALID as id,
+                PERSONAL_Nombre as nombre,
+                PERSONAL_Puesto as puesto
+            FROM tblPersonal
+            WHERE PERSONAL_Archivado = ?
+            $searchCondition
+            ORDER BY $sortBy $sortOrder
+            OFFSET CAST(? AS INT) ROWS
+            FETCH NEXT CAST(? AS INT) ROWS ONLY",
+            $params
+        )->getAll();
+
+        return [
+            "data" => $results,
+            "pagination" => [
+                "totalItems" => $totalCount,
+                "totalPages" => $totalPages,
+                "currentPage" => $page,
+            ]
+        ];
+    }
+
     public function getById($personalId) {
         return $this->query(
             "SELECT * FROM 
