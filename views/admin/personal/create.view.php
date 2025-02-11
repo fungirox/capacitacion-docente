1b03b6e1c8a7a7073fe1f12fe648d0ecae46c54f
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-11 10:56:50 -0700

    Vista para agregar un personal

---
+++ views/admin/personal/create.view.php
@@ -0,0 +1,45 @@
+<?php view("components/styledHeader.php", ["title" => $title]); ?>
+<script defer>
+    $(document).ready(function() {
+        $('input').on('input', function() {
+            $(this).removeClass('is-invalid');
+        });
+    });
+</script>
+<main role="main" class="container py-5" style="margin-top: 56px">
+    <?php view("components/nestedTitle.php", ["url" => "/admin/personal", "title" => $title]) ?>
+    <form class="row mt-4" method="POST" action="/admin/personal">
+        <div class="col-12 mb-3">
+            <label for="nombre" class="form-label">Nombre completo</label>
+            <input type="text" class="form-control <?= isValidInput($errors, "nombre") ?>" id="nombre" name="nombre" value="<?= cleanOld("nombre") ?>">
+            <div class="invalid-feedback">
+                <?= $errors['nombre'] ?>
+            </div>
+        </div>
+        <div class="col-12 mb-3">
+            <label for="puesto" class="form-label">Puesto</label>
+            <input type="text" class="form-control <?= isValidInput($errors, "puesto") ?>" id="puesto" name="puesto" value="<?= cleanOld("puesto") ?>">
+            <div class="invalid-feedback">
+                <?= $errors['puesto'] ?>
+            </div>
+        </div>
+        <div class="col-md-12 mb-4">
+            <label for="titulo" class="form-label">Título</label>
+            <select class="form-select <?= isValidInput($errors, "titulo") ?>" id="titulo" name="titulo">
+                <?php foreach ($titulos as $key => $titulo): ?>
+                    <option value="<?= $key ?>" <?= cleanOld("titulo") === $key ? "selected" : "" ?>><?= $titulo ?></option>
+                <?php endforeach; ?>
+            </select>
+            <div class="invalid-feedback">
+                <?= $errors['titulo'] ?>
+            </div>
+        </div>
+        <div class="order-2 order-md-1 col-md-auto col-12 ms-md-auto mb-3">
+            <div class="d-grid"><a href="/admin/personal" class="btn btn-outline-secondary">Cancelar</a></div>
+        </div>
+        <div class="order-1 order-md-2 col-md-auto col-12 mb-3">
+            <div class="d-grid"><button type="submit" class="btn btn-primary">Agregar</button></div>
+        </div>
+    </form>
+</main>
+<?php view("components/styledFooter.php"); ?>
