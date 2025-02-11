d35fc4d4160a61bcfa444c3601ec358fac2c17d5
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-11 11:33:26 -0700

    Se agrega personal

---
+++ Http/controllers/admin/personal/store.php
@@ -0,0 +1,15 @@
+<?php
+
+use Core\App;
+use Core\Repositories\PersonalRepository;
+use Http\Forms\PersonalForm;
+
+PersonalForm::validate($attributes = [
+    "nombre" => trim($_POST["nombre"]),
+    "puesto" => trim($_POST["puesto"]),
+    "titulo" => trim($_POST["titulo"])
+]);
+
+App::resolve(PersonalRepository::class)->create($attributes);
+
+redirect("/admin/personal");
