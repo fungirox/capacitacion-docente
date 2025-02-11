bc629f48277165f653ad5da25ab4c589c0bb3f94
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-10 17:41:52 -0700

    Controlador para cargar la vista del index de asistencia

--- Http/controllers/instructor/instruyendo/asistencia/index.php
+++ Http/controllers/instructor/instruyendo/asistencia/index.php
@@ -1,1 +1,12 @@
 <?php
+
+use Core\App;
+use Core\Repositories\CursoDocenteRepository;
+
+$alumnos = App::resolve(CursoDocenteRepository::class)->getDocentesFromCurso($_GET["id"]);
+
+return view("/instructor/instruyendo/asistencia/index.view.php", [
+    "title" => "Asistencia",
+    "alumnos" => $alumnos
+]);
+
