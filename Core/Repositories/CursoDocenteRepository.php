470bcd65c259791e3c95d2aea1928e5424786bca
Diego Rascón <jose.d.rascon@gmail.com>
2025-02-10 20:53:22 -0700

    Ya no se revisa la propiedad del usuario

--- Core/Repositories/CursoDocenteRepository.php
+++ Core/Repositories/CursoDocenteRepository.php
@@ -4,20 +4,17 @@ namespace Core\Repositories;
 
 class CursoDocenteRepository extends RepositoryTemplate {
 
-    public function getDocentesFromCurso($cursoId, $userId) {
+    public function getDocentesFromCurso($cursoId) {
         return $this->query(
             "SELECT
                 docente.DOCENTEID AS id,
                 USER_Nombre + ' ' + USER_Apellido AS nombre
             FROM tblCursoDocente AS curso_docente
-                INNER JOIN tblDocente AS docente ON curso_docente.DOCENTEID = docente.DOCENTEID
-                INNER JOIN tblUsuario AS usuario ON docente.USERID = usuario.USERID
-                INNER JOIN tblCursoInstructor AS curso_instructor ON curso_instructor.CURSOID = curso_docente.CURSOID
-                INNER JOIN tblInstructor AS instructor ON instructor.INSTRUCTORID = curso_instructor.INSTRUCTORID
-            WHERE curso_docente.CURSOID = ? AND
-                instructor.USERID = ?
+            INNER JOIN tblDocente AS docente ON curso_docente.DOCENTEID = docente.DOCENTEID
+            INNER JOIN tblUsuario AS usuario ON docente.USERID = usuario.USERID
+            WHERE curso_docente.CURSOID = ? 
             ORDER BY nombre ASC",
-            [$cursoId, $userId]
+            [$cursoId]
         )->getAll();
     }
 
