73ff8abb48f8c0a8a62f2afccb02e85b51993279
Diego Rascón <jose.d.rascon@gmail.com>
2025-03-24 23:22:58 -0700

    Se borró el repositorio de carreras

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,6 @@ use Core\Database;
 use Core\Repositories\AdministradorRepository;
 use Core\Repositories\AreaRepository;
 use Core\Repositories\AsistenciaCursoRepository;
-use Core\Repositories\CarreraRepository;
 use Core\Repositories\CursoDocenteRepository;
 use Core\Repositories\CursoRepository;
 use Core\Repositories\DocenteRepository;
@@ -31,10 +30,6 @@ $container->bind("Core\Repositories\AdministradorRepository", function () {
 
 $container->bind("Core\Repositories\AreaRepository", function () {
     return new AreaRepository(App::resolve(Database::class));
-});
-
-$container->bind("Core\Repositories\CarreraRepository", function () {
-    return new CarreraRepository(App::resolve(Database::class));
 });
 
 $container->bind("Core\Repositories\CursoDocenteRepository", function () {
